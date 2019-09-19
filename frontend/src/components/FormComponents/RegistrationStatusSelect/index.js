2413adc5ef0e7624b1f314688c76163eed9815a5
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-09-19 13:25:12 +0300

    Admin & tools page, team page improvements

--- frontend/src/components/FormComponents/RegistrationStatusSelect/index.js
+++ frontend/src/components/FormComponents/RegistrationStatusSelect/index.js
@@ -3,9 +3,16 @@ import React from 'react';
 import { RegistrationStatuses } from '@hackjunction/shared';
 import { Select, Tag } from 'antd';
 
-const RegistrationStatusSelect = ({ value, onChange, placeholder = 'Select status' }) => {
+const RegistrationStatusSelect = ({ value, onChange, placeholder = 'Select status', selectProps }) => {
     return (
-        <Select placeholder={placeholder} size="large" value={value} style={{ width: '100%' }} onChange={onChange}>
+        <Select
+            placeholder={placeholder}
+            size="large"
+            value={value}
+            style={{ width: '100%', position: 'relative' }}
+            onChange={onChange}
+            {...selectProps}
+        >
             <Select.OptGroup label="Can assign">
                 {RegistrationStatuses.asArray
                     .filter(status => status.allowAssign)
---
+++ frontend/src/pages/OrganiserDashboard/OrganiserEditEvent/OrganiserEditEventReview/AdminPage.js
@@ -0,0 +1,157 @@
+import React, { useMemo } from 'react';
+import './AdminPage.scss';
+
+import { connect } from 'react-redux';
+import { groupBy, filter } from 'lodash-es';
+import { RegistrationStatuses } from '@hackjunction/shared';
+import { Row, Col, Card, Statistic, Tag, List, Button as AntButton } from 'antd';
+import Divider from 'components/generic/Divider';
+import * as OrganiserSelectors from 'redux/organiser/selectors';
+
+const STATUSES = RegistrationStatuses.asObject;
+
+const AdminPage = ({ registrations }) => {
+    const groupedByStatus = useMemo(() => {
+        return groupBy(registrations, 'status');
+    }, [registrations]);
+
+    const getCount = (statuses = []) => {
+        return statuses.reduce((res, status) => {
+            if (groupedByStatus.hasOwnProperty(status)) {
+                return res + groupedByStatus[status].length;
+            }
+            return res;
+        }, 0);
+    };
+
+    const total = registrations.length;
+    const rated = filter(registrations, reg => reg.rating).length;
+    const ratedOrAssigned = filter(registrations, reg => reg.rating || reg.assignedTo).length;
+
+    const ACTIONS = [
+        {
+            title: 'Accept all soft accepted',
+            description:
+                'Change the status of all Soft Accepted participants to Accepted, and notify them via email that they have been accepted to the event!',
+            extra: (
+                <AntButton onClick={() => window.alert('Get permission from Juuso to do this ;--)')} type="link">
+                    Accept
+                </AntButton>
+            )
+        },
+        {
+            title: 'Reject all soft rejected',
+            description:
+                'Change the status of all Soft Rejected participants to Rejected, and notify them via email that they did not make it.',
+            extra: (
+                <AntButton onClick={() => window.alert('Get permission from Juuso to do this ;--)')} type="link">
+                    Reject
+                </AntButton>
+            )
+        }
+    ];
+
+    console.log('RATED', rated);
+
+    return (
+        <React.Fragment>
+            <Divider size={1} />
+            <Row gutter={16}>
+                <Col xs={24} md={8}>
+                    <Divider size={1} />
+                    <Card title="Total registrations">
+                        <Statistic title="" value={total}></Statistic>
+                    </Card>
+                </Col>
+                <Col xs={24} md={8}>
+                    <Divider size={1} />
+                    <Card title="Rated">
+                        <Statistic title="" value={`${rated} / ${total}`}></Statistic>
+                    </Card>
+                </Col>
+                <Col xs={24} md={8}>
+                    <Divider size={1} />
+                    <Card title="Rated or Assigned">
+                        <Statistic title="" value={`${ratedOrAssigned} / ${total}`}></Statistic>
+                    </Card>
+                </Col>
+                <Col xs={24}>
+                    <Divider size={1} />
+                    <Card title="By status" bodyStyle={{ padding: 0 }}>
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.pending.color}>{STATUSES.pending.label}</Tag>
+                                    <Statistic value={getCount(['pending'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.softAccepted.color}>{STATUSES.softAccepted.label}</Tag>
+                                    <Statistic value={getCount(['softAccepted'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.accepted.color}>{STATUSES.accepted.label}</Tag>
+                                    <Statistic value={getCount(['accepted'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.softRejected.color}>{STATUSES.softRejected.label}</Tag>
+                                    <Statistic value={getCount(['softRejected'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.rejected.color}>{STATUSES.rejected.label}</Tag>
+                                    <Statistic value={getCount(['rejected'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.confirmed.color}>{STATUSES.confirmed.label}</Tag>
+                                    <Statistic value={getCount(['confirmed'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.checkedIn.color}>{STATUSES.checkedIn.label}</Tag>
+                                    <Statistic value={getCount(['checkedIn'])} />
+                                </Card.Grid>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card.Grid style={{ width: '100%' }}>
+                                    <Tag color={STATUSES.noShow.color}>{STATUSES.noShow.label}</Tag>
+                                    <Statistic value={getCount(['noShow'])} />
+                                </Card.Grid>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col xs={24}>
+                    <Divider size={1} />
+                    <Card title="Actions">
+                        <List>
+                            <List
+                                itemLayout="horizontal"
+                                dataSource={ACTIONS}
+                                renderItem={item => (
+                                    <List.Item extra={item.extra}>
+                                        <List.Item.Meta title={item.title} description={item.description} />
+                                    </List.Item>
+                                )}
+                            />
+                        </List>
+                    </Card>
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
+};
+
+const mapState = state => ({
+    registrations: OrganiserSelectors.registrations(state)
+});
+export default connect(mapState)(AdminPage);
