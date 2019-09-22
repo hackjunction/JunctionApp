2c6376ffaf2c9668444d113f8e068bd490905579
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-09-22 18:34:42 +0300

    Fix filters

--- frontend/src/pages/OrganiserDashboard/OrganiserEditEvent/OrganiserEditEventReview/TeamsPage.js
+++ frontend/src/pages/OrganiserDashboard/OrganiserEditEvent/OrganiserEditEventReview/TeamsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import styles from './TeamsPage.module.scss';
 
 import { connect } from 'react-redux';
