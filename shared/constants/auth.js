f99c68175d499bed6d2d8d0ee76365384add9be6
Gergely Bihary <biharygergo@gmail.com>
2019-09-30 15:57:28 +0300

    Added new roles and permissions to the constants

--- shared/constants/auth.js
+++ shared/constants/auth.js
@@ -1,13 +1,15 @@
 const Roles = {
     SUPER_ADMIN: 'SuperAdmin',
     ORGANISER: 'Organiser',
-    ASSISTANT_ORGANISER: 'AssistantOrganiser'
+    ASSISTANT_ORGANISER: 'AssistantOrganiser',
+    RECRUITER: 'Recruiter'
 };
 
 const Permissions = {
     CREATE_EVENT: 'create:event',
     MANAGE_EVENT: 'manage:event',
-    DELETE_EVENT: 'delete:event'
+    DELETE_EVENT: 'delete:event',
+    ACCESS_RECRUITMENT: 'access:recruitment'
 };
 
 module.exports = {
