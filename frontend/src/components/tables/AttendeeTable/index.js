466143b08684e895bf038c68b4118e78b51143c4
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-02 18:02:35 +0300

    Add material-table

--- frontend/src/components/tables/AttendeeTable/index.js
+++ frontend/src/components/tables/AttendeeTable/index.js
@@ -8,6 +8,7 @@ import { RegistrationStatuses } from '@hackjunction/shared';
 import EmailIcon from '@material-ui/icons/Email';
 import EditIcon from '@material-ui/icons/Edit';
 import Table from 'components/generic/Table';
+import MaterialTable from 'components/generic/MaterialTable';
 
 import * as OrganiserSelectors from 'redux/organiser/selectors';
 import EditRegistrationModal from 'components/modals/EditRegistrationModal';
@@ -21,66 +22,47 @@ const AttendeeTable = ({ organiserProfilesMap, emptyRenderer, event, loading, at
         }
 
         return (
-            <Table
-                dataSource={attendees}
-                rowKey="user"
-                loading={loading}
-                title={`${attendees.length} results`}
-                footer={footer}
-                selectedActions={[
-                    {
-                        key: 'edit',
-                        label: 'Edit all',
-                        icon: <EditIcon />,
-                        action: items => window.alert('Bulk edit temporarily unavailable')
-                    },
-                    {
-                        key: 'email',
-                        label: 'Email all',
-                        icon: <EmailIcon />,
-                        action: items => window.alert('Bulk email temporarily unavailable')
-                    }
-                ]}
-                rowActions={[
-                    {
-                        key: 'edit',
-                        label: 'Edit',
-                        action: item => setEditing(item._id)
-                    }
-                ]}
+            <MaterialTable
+                isLoading={loading}
+                data={attendees}
+                onRowClick={(e, row) => setEditing(row._id)}
+                options={{
+                    exportButton: true
+                }}
                 columns={[
                     {
-                        key: 'name',
-                        path: 'answers',
-                        label: 'Name',
-                        render: answers => `${answers.firstName} ${answers.lastName}`
+                        title: 'First name',
+                        field: 'answers.firstName',
+                        searchable: true
                     },
                     {
-                        key: 'email',
-                        path: 'answers.email',
-                        label: 'Email'
+                        title: 'Last name',
+                        field: 'answers.lastName',
+                        searchable: true
                     },
                     {
-                        key: 'rating',
-                        path: 'rating',
-                        label: 'Rating',
-                        render: rating => rating || 'Pending'
+                        title: 'Email',
+                        field: 'answers.email',
+                        searchable: true
                     },
                     {
-                        key: 'status',
-                        path: 'status',
-                        label: 'Status',
-                        render: status => {
-                            const params = RegistrationStatuses.asObject[status];
+                        title: 'Rating',
+                        field: 'rating'
+                    },
+                    {
+                        title: 'Status',
+                        field: 'status',
+                        render: row => {
+                            const params = RegistrationStatuses.asObject[row.status];
                             if (!params) return '-';
                             return <Tag color={params.color}>{params.label}</Tag>;
                         }
                     },
                     {
-                        key: 'tags',
-                        path: 'tags',
-                        label: 'Tags',
-                        render: tags => {
+                        title: 'Tags',
+                        field: 'tags',
+                        render: row => {
+                            const { tags } = row;
                             if (!tags || !tags.length) {
                                 return '-';
                             } else {
@@ -97,24 +79,25 @@ const AttendeeTable = ({ organiserProfilesMap, emptyRenderer, event, loading, at
                         }
                     },
                     {
-                        key: 'createdAt',
-                        path: 'createdAt',
-                        label: 'Submitted',
-                        render: date => moment(date).fromNow()
+                        title: 'Submitted',
+                        field: 'createdAt',
+                        render: row => moment(row.createdAt).format('MMM Do YYYY HH:mm:ss'),
+                        sorting: true,
+                        type: 'datetime'
                     },
                     {
-                        key: 'assignedTo',
-                        path: 'assignedTo',
-                        label: 'Assigned to',
-                        render: (userId, record) => {
+                        title: 'Assigned to',
+                        field: 'assignedTo',
+                        render: row => {
+                            const userId = row.assignedTo;
                             let text;
                             if (!userId) {
-                                text = 'No one';
+                                text = '-';
                             } else if (organiserProfilesMap.hasOwnProperty(userId)) {
                                 const user = organiserProfilesMap[userId];
                                 text = `${user.firstName} ${user.lastName}`;
                             } else {
-                                text = 'Unknown user';
+                                text = '???';
                             }
                             return text;
                         }
@@ -122,6 +105,109 @@ const AttendeeTable = ({ organiserProfilesMap, emptyRenderer, event, loading, at
                 ]}
             />
         );
+
+        // return (
+        //     <Table
+        //         dataSource={attendees}
+        //         rowKey="user"
+        //         loading={loading}
+        //         title={`${attendees.length} results`}
+        //         footer={footer}
+        //         selectedActions={[
+        //             {
+        //                 key: 'edit',
+        //                 label: 'Edit all',
+        //                 icon: <EditIcon />,
+        //                 action: items => window.alert('Bulk edit temporarily unavailable')
+        //             },
+        //             {
+        //                 key: 'email',
+        //                 label: 'Email all',
+        //                 icon: <EmailIcon />,
+        //                 action: items => window.alert('Bulk email temporarily unavailable')
+        //             }
+        //         ]}
+        //         rowActions={[
+        //             {
+        //                 key: 'edit',
+        //                 label: 'Edit',
+        //                 action: item => setEditing(item._id)
+        //             }
+        //         ]}
+        //         columns={[
+        //             {
+        //                 key: 'name',
+        //                 path: 'answers',
+        //                 label: 'Name',
+        //                 render: answers => `${answers.firstName} ${answers.lastName}`
+        //             },
+        //             {
+        //                 key: 'email',
+        //                 path: 'answers.email',
+        //                 label: 'Email'
+        //             },
+        //             {
+        //                 key: 'rating',
+        //                 path: 'rating',
+        //                 label: 'Rating',
+        //                 render: rating => rating || 'Pending'
+        //             },
+        //             {
+        //                 key: 'status',
+        //                 path: 'status',
+        //                 label: 'Status',
+        //                 render: status => {
+        //                     const params = RegistrationStatuses.asObject[status];
+        //                     if (!params) return '-';
+        //                     return <Tag color={params.color}>{params.label}</Tag>;
+        //                 }
+        //             },
+        //             {
+        //                 key: 'tags',
+        //                 path: 'tags',
+        //                 label: 'Tags',
+        //                 render: tags => {
+        //                     if (!tags || !tags.length) {
+        //                         return '-';
+        //                     } else {
+        //                         return event.tags
+        //                             .filter(tag => {
+        //                                 return tags.indexOf(tag.label) !== -1;
+        //                             })
+        //                             .map(({ color, label }) => (
+        //                                 <Tag key={label} color={color}>
+        //                                     {label}
+        //                                 </Tag>
+        //                             ));
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 key: 'createdAt',
+        //                 path: 'createdAt',
+        //                 label: 'Submitted',
+        //                 render: date => moment(date).fromNow()
+        //             },
+        //             {
+        //                 key: 'assignedTo',
+        //                 path: 'assignedTo',
+        //                 label: 'Assigned to',
+        //                 render: (userId, record) => {
+        //                     let text;
+        //                     if (!userId) {
+        //                         text = 'No one';
+        //                     } else if (organiserProfilesMap.hasOwnProperty(userId)) {
+        //                         const user = organiserProfilesMap[userId];
+        //                         text = `${user.firstName} ${user.lastName}`;
+        //                     } else {
+        //                         text = 'Unknown user';
+        //                     }
+        //                     return text;
+        //                 }
+        //             }
+        //         ]}
+        //     />
+        // );
     };
 
     const renderEmpty = () => {
