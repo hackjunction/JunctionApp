61ba11f8ba575302aaec3cd3d98a3dd10b95077c
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-03 10:41:45 +0300

    Add skeleton components for search

---
+++ frontend/src/pages/Recruitment/Search/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+import PageWrapper from 'components/PageWrapper';
+import SearchBox from './SearchBox';
+import SearchResults from './SearchResults';
+
+const SearchPage = () => {
+    return(
+        <PageWrapper>
+            <SearchBox />
+            <SearchResults />
+        </PageWrapper>
+    );
+}
+
+export default SearchPage;
