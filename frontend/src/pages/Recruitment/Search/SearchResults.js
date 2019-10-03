6539cb406729c66b823c411ddcbc1d773758a72a
petrus <holm.petrus@gmail.com>
2019-10-03 11:24:21 +0300

    Initial draft for Recruitment parts page

--- frontend/src/pages/Recruitment/Search/SearchResults.js
+++ frontend/src/pages/Recruitment/Search/SearchResults.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RecruitmentUserModal from 'components/modals/RecruitmentUserModal';
 
 const SearchResults = () => {
-    return null;
-}
+  const [selected, setSelected] = useState();
+
+  useEffect(() => {
+    setSelected('5d5a860fdc07b6002b2329d2');
+  });
+
+  const event = {};
+
+  return (
+    <RecruitmentUserModal
+      registrationId={selected}
+      event={event}
+      idToken={
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpCI6Ik5FWkZOa1U1TWtZMk1EWkNRelZGUVRGQ1JVWkZOVGRDUkVVNFF6SkdSRVZFTTBSRFJrWXdSZyJ9'
+      }
+    />
+  );
+};
 
 export default SearchResults;
