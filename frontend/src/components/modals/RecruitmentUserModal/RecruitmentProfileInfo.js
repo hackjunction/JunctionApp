6320f2da9c30f3134d0764ba6edb05d4fa92c10e
petrus <holm.petrus@gmail.com>
2019-10-08 00:17:48 +0300

    Flex layout, add textarea for contact

--- frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
+++ frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
@@ -2,57 +2,41 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { RegistrationFields } from '@hackjunction/shared';
 import { groupBy } from 'lodash-es';
-import {
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  Typography,
-  Link
-} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Typography, Link, Box } from '@material-ui/core';
 
-import Image from 'components/generic/Image';
+import { Input } from 'antd';
 
 const RecruitmentProfileInfo = React.memo(({ profile }) => {
   const fields = Object.keys(profile);
   const grouped = groupBy(fields, field =>
     RegistrationFields.getCategory(field)
   );
-  const { education, portfolio, spokenLanguages, avatar } = profile;
+  const { education, portfolio, spokenLanguages, firstName } = profile;
   return (
     <React.Fragment>
-      {education && (
-        <React.Fragment>
-          <Typography variant="h4">Education</Typography>
-          <Typography>{education.level}</Typography>
-        </React.Fragment>
-      )}
-      {portfolio && (
-        <React.Fragment>
-          <Typography variant="h4">Portfolio</Typography>
-          <Link>{portfolio}</Link>
-        </React.Fragment>
-      )}
-
-      <Typography variant="h4">Interests</Typography>
-      <Link>{portfolio}</Link>
-      <ExpansionPanel key="languages">
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="languages-content"
-          id="languages-header"
-        >
-          <Typography>Languages</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          {spokenLanguages.map(language => (
-            <p>
-              <Typography>{language}</Typography>
-            </p>
-          ))}
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <textarea></textarea>
+      <Box width="30%">
+        {education && (
+          <React.Fragment>
+            <Typography variant="h6">Education</Typography>
+            <Typography>{education.level}</Typography>
+          </React.Fragment>
+        )}
+        {portfolio && (
+          <React.Fragment>
+            <Typography variant="h6">Portfolio</Typography>
+            <Link>{portfolio}</Link>
+          </React.Fragment>
+        )}
+        <Typography variant="h6">Interests</Typography>
+        <Link>{portfolio}</Link>
+      </Box>
+      <Box>
+        <Typography>Contact {firstName}</Typography>
+        <Input.TextArea
+          autosize={{ minRows: 10, maxRows: 20 }}
+          placeholder="Max 1000 characters"
+        />
+      </Box>
     </React.Fragment>
   );
 });
