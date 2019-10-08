63c259e3b97e072b6343e4519ce67e75804a8cb7
petrus <holm.petrus@gmail.com>
2019-10-08 16:26:49 +0300

    added changes to profile view

--- frontend/src/services/userProfiles.js
+++ frontend/src/services/userProfiles.js
@@ -3,49 +3,46 @@ import _axios from 'services/axios';
 const UserProfilesService = {};
 
 function config(idToken) {
-    return {
-        headers: {
-            Authorization: `Bearer ${idToken}`
-        }
-    };
+  return {
+    headers: {
+      Authorization: `Bearer ${idToken}`
+    }
+  };
 }
 
 UserProfilesService.getPublicUserProfiles = userIds => {
-    return _axios.get('/user-profiles/public/', {
-        params: {
-            userIds
-        }
-    });
+  return _axios.get('/user-profiles/public/', {
+    params: {
+      userIds
+    }
+  });
 };
 
 UserProfilesService.getUserProfile = idToken => {
-    return _axios.get('/user-profiles/', config(idToken));
+  return _axios.get('/user-profiles/', config(idToken));
 };
 
 UserProfilesService.createUserProfile = (data, idToken) => {
-    return _axios.post('/user-profiles/', data, config(idToken));
+  return _axios.post('/user-profiles/', data, config(idToken));
 };
 
 UserProfilesService.updateUserProfile = (data, idToken) => {
-    return _axios.patch('/user-profiles/', data, config(idToken));
+  return _axios.patch('/user-profiles/', data, config(idToken));
 };
 
 UserProfilesService.getUsersByEmail = (email, idToken) => {
-    return _axios.get('/user-profiles/search', {
-        params: { email },
-        ...config(idToken)
-    });
+  return _axios.get('/user-profiles/search', {
+    params: { email },
+    ...config(idToken)
+  });
 };
 
 UserProfilesService.queryUsers = idToken => {
-    return _axios.get('/recruitment/search', config(idToken));
+  return _axios.get('/recruitment/search', config(idToken));
 };
 
 UserProfilesService.getUserProfileRecruitment = (userId, idToken) => {
-    return _axios.get('/user-profiles/recruitment/profile', {
-        params: { userId },
-        ...config(idToken)
-    });
+  return _axios.get(`/recruitment/profile/${userId}`, config(idToken));
 };
 
 export default UserProfilesService;
