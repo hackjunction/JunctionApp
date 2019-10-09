743f2025ec587a0f73ea4502bd19e2c4f0d93615
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-09 15:57:07 +0300

    Add stats page by filter group

---
+++ frontend/src/components/plots/RegistrationsByCountry.js
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+
+import { Box, Typography } from '@material-ui/core';
+import { groupBy, sortBy } from 'lodash-es';
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, CartesianGrid, Bar } from 'recharts';
+
+const RegistrationsByCountry = ({ registrations }) => {
+    const formattedData = useMemo(() => {
+        const byCountry = groupBy(registrations, 'answers.countryOfTravel');
+        const mapped = Object.keys(byCountry).map(country => ({
+            country,
+            count: byCountry[country].length
+        }));
+
+        return sortBy(mapped, item => -1 * item.count);
+    }, [registrations]);
+
+    return (
+        <Box p={2}>
+            <Typography paragraph variant="h6" align="center">
+                By country of travel
+                <Typography variant="subtitle1">{formattedData.length} countries</Typography>
+            </Typography>
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart
+                    data={formattedData}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="country" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="count" fill="orange" />
+                </BarChart>
+            </ResponsiveContainer>
+        </Box>
+    );
+};
+
+export default RegistrationsByCountry;
---
+++ frontend/src/components/plots/RegistrationsByGender.js
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+
+import { Box, Typography } from '@material-ui/core';
+import { groupBy, sortBy } from 'lodash-es';
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, CartesianGrid, Bar } from 'recharts';
+
+const RegistrationsByNationality = ({ registrations }) => {
+    const formattedData = useMemo(() => {
+        const byGender = groupBy(registrations, 'answers.gender');
+        const mapped = Object.keys(byGender).map(gender => ({
+            gender,
+            count: byGender[gender].length
+        }));
+
+        return sortBy(mapped, item => -1 * item.count);
+    }, [registrations]);
+
+    return (
+        <Box p={2}>
+            <Typography paragraph variant="h6" align="center">
+                By gender
+                <Typography variant="subtitle1">{formattedData.length} genders</Typography>
+            </Typography>
+            <ResponsiveContainer width="100%" height={400}>
+                <BarChart
+                    data={formattedData}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="gender" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="count" fill="orange" />
+                </BarChart>
+            </ResponsiveContainer>
+        </Box>
+    );
+};
+
+export default RegistrationsByNationality;
