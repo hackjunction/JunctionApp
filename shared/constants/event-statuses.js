fd47dbbc3a391df5b63f2cca45d6fa89168ae8ef
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-10 17:06:43 +0300

    Refactor event detail page to material-ui

---
+++ shared/constants/event-statuses.js
@@ -0,0 +1,28 @@
+const EventStatuses = {
+    PUBLISHED: {
+        id: 'PUBLISHED',
+        label: 'Published'
+    },
+    REGISTRATION_OPEN: {
+        id: 'REGISTRATION_OPEN',
+        label: 'Registration open'
+    },
+    REGISTRATION_ENDED: {
+        id: 'REGISTRATION_ENDED',
+        label: 'Registration ended'
+    },
+    WEEK_OF_EVENT: {
+        id: 'WEEK_OF_EVENT',
+        label: 'Week of event'
+    },
+    IN_PROGRESS: {
+        id: 'IN_PROGRESS',
+        label: 'In progress'
+    },
+    FINISHED: {
+        id: 'FINISHED',
+        label: 'Finished'
+    }
+};
+
+module.exports = EventStatuses;
