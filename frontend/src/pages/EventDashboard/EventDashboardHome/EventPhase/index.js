2651b407553f24f4b1cbbdf95d2a59a1ad8599b6
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-11 15:28:45 +0300

    Fix cancel button

--- frontend/src/pages/EventDashboard/EventDashboardHome/EventPhase/index.js
+++ frontend/src/pages/EventDashboard/EventDashboardHome/EventPhase/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+import CheckInBlock from './CheckInBlock';
+
 const EventPhase = () => {
-    return <div>Event phase</div>;
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <CheckInBlock />
+            </Grid>
+        </Grid>
+    );
 };
 
 export default EventPhase;
