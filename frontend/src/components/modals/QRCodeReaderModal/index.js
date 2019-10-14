eb270337e30b3016604e0609a7c59e86de38a4e5
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-14 17:29:09 +0300

    Allow checkins via QR code reader modal

--- frontend/src/components/modals/QRCodeReaderModal/index.js
+++ frontend/src/components/modals/QRCodeReaderModal/index.js
@@ -1,36 +1,136 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import QrReader from 'react-qr-reader';
 
-import { Box, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Typography, Avatar, Button, CircularProgress } from '@material-ui/core';
 import { connect } from 'react-redux';
+import { withSnackbar } from 'notistack';
+import { Tag } from 'antd';
+import { RegistrationStatuses } from '@hackjunction/shared';
 
 import Modal from 'components/generic/Modal';
 import * as OrganiserSelectors from 'redux/organiser/selectors';
+import * as OrganiserActions from 'redux/organiser/actions';
+import UserProfilesService from 'services/userProfiles';
 
-const QRCodeReaderModal = ({ open, onClose, registrationsMap }) => {
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        width: 200,
+        height: 200,
+        margin: theme.spacing(2)
+    }
+}));
+
+const QRCodeReaderModal = ({ open, onClose, enqueueSnackbar, registrationsMap, event, editRegistration }) => {
+    const classes = useStyles();
     const [user, setUser] = useState();
-    const handleError = () => {
-        window.alert('ERROR!');
+    const [loading, setLoading] = useState(false);
+
+    const handleError = useCallback(() => {
+        enqueueSnackbar('Error scanning QR code', { variant: 'error' });
+    }, [enqueueSnackbar]);
+
+    const handleCheckIn = useCallback(() => {
+        setLoading(true);
+        const registration = registrationsMap[user.userId];
+        editRegistration(registration._id, { status: RegistrationStatuses.asObject.checkedIn.id }, event.slug)
+            .then(() => {
+                enqueueSnackbar('Changed status to checked in', { variant: 'success' });
+                // setUser();
+            })
+            .catch(err => {
+                enqueueSnackbar('Something went wrong', { variant: 'error' });
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [editRegistration, event, registrationsMap, enqueueSnackbar, user]);
+
+    const handleScan = useCallback(
+        data => {
+            if (data) {
+                setLoading(true);
+                UserProfilesService.getPublicUserProfiles(data)
+                    .then(users => {
+                        if (users.length === 0) {
+                            enqueueSnackbar('Unknown user', { variant: 'warning' });
+                        } else {
+                            setUser(users[0]);
+                        }
+                    })
+                    .catch(err => {
+                        enqueueSnackbar('Unknown error', { variant: 'error' });
+                    })
+                    .finally(() => {
+                        setLoading(false);
+                    });
+            }
+        },
+        [enqueueSnackbar]
+    );
+
+    const renderRegistrationInfo = () => {
+        const registration = registrationsMap[user.userId];
+        if (!registration) {
+            return (
+                <React.Fragment>
+                    <Typography variant="subtitle1" color="secondary" align="center">
+                        No registration found!
+                    </Typography>
+                </React.Fragment>
+            );
+        }
+        const status = RegistrationStatuses.asObject[registration.status];
+
+        return (
+            <React.Fragment>
+                <Typography variant="subtitle1" color="textSecondary" align="center">
+                    Registration status: <br />
+                    <Tag color={status.color}>{status.label}</Tag>
+                </Typography>
+                <Box mt={2} />
+                {renderActions(status)}
+            </React.Fragment>
+        );
     };
 
-    const handleScan = data => {
-        if (registrationsMap.hasOwnProperty(data)) {
-            const _user = registrationsMap[data];
-            setUser(_user.answers.firstName + ' ' + _user.answers.lastName);
+    const renderActions = status => {
+        if (!status) return null;
+        switch (status.id) {
+            case RegistrationStatuses.asObject.confirmed.id: {
+                return (
+                    <Button color="primary" variant="contained" onClick={handleCheckIn}>
+                        Check in
+                    </Button>
+                );
+            }
+            default:
+                return (
+                    <Button color="primary" variant="contained" onClick={() => setUser()}>
+                        Scan again
+                    </Button>
+                );
         }
     };
 
     if (!open) return null;
 
-    console.log('USER', user);
-
     return (
         <Modal isOpen={true} handleClose={onClose} size="max">
             <Box p={5} display="flex" height="100%" flexDirection="column" alignItems="center" justifyContent="center">
-                {user ? (
-                    <Box>
-                        <Typography>{user}</Typography>
-                        <Button onClick={() => setUser(null)}>Scan again</Button>
+                {loading ? (
+                    <CircularProgress size={24} />
+                ) : user ? (
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <Avatar
+                            alt={`${user.firstName} ${user.lastName}`}
+                            src={user.avatar}
+                            className={classes.avatar}
+                        />
+                        <Typography variant="h6" align="center">
+                            {user.firstName} {user.lastName}
+                        </Typography>
+                        {renderRegistrationInfo()}
                     </Box>
                 ) : (
                     <QrReader
@@ -47,7 +147,18 @@ const QRCodeReaderModal = ({ open, onClose, registrationsMap }) => {
 };
 
 const mapState = state => ({
-    registrationsMap: OrganiserSelectors.registrationsMap(state)
+    registrationsMap: OrganiserSelectors.registrationsMap(state),
+    event: OrganiserSelectors.event(state)
 });
 
-export default connect(mapState)(QRCodeReaderModal);
+const mapDispatch = dispatch => ({
+    editRegistration: (registrationId, data, slug) =>
+        dispatch(OrganiserActions.editRegistration(registrationId, data, slug))
+});
+
+export default withSnackbar(
+    connect(
+        mapState,
+        mapDispatch
+    )(QRCodeReaderModal)
+);
