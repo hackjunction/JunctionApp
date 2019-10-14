a0f474c02d80cb5ed30d08d9cae39c7d85b69676
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-14 15:55:43 +0300

    Mock skills & roles data

--- frontend/src/redux/recruitment/selectors.js
+++ frontend/src/redux/recruitment/selectors.js
@@ -1,4 +1,33 @@
+import { createSelector } from 'reselect';
+import { Skills, Roles } from '@hackjunction/shared';
+
 export const searchResults = state => state.recruitment.searchResults.data;
+
+export const searchResultsMapped = createSelector(
+    searchResults,
+    searchResults => {
+        return searchResults.map(item => {
+            // Skills: generate 1-5 random skills, and a level from 1-5 for each
+            item.skills = [
+                {
+                    skill: 'Javascript', //shared/constants/skills.js
+                    level: 5 //1-5
+                }
+            ];
+
+            // Roles: generate 1-5 random roles, and a 'years' value from 1-5 for each
+            item.roles = [
+                {
+                    role: 'Fullstack Developer', //shared/constants/roles.js
+                    years: 3 //1-5
+                }
+            ];
+
+            return item;
+        });
+    }
+);
+
 export const searchResultsLoading = state => state.recruitment.searchResults.loading;
 export const searchResultsError = state => state.recruitment.searchResults.error;
 export const searchResultsUpdated = state => state.recruitment.searchResults.updated;
