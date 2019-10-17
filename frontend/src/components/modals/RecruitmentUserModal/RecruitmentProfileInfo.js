91cbc0d9a30ffadd7db9a50eb298ea4e252a4778
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-17 14:53:56 +0300

    Use recruitment api route in profile detail view

--- frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
+++ frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
@@ -2,59 +2,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { RegistrationFields } from '@hackjunction/shared';
 import { groupBy } from 'lodash-es';
-import {
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  Typography,
-  Link
-} from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, Link } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import Image from 'components/generic/Image';
 
 const RecruitmentProfileInfo = React.memo(({ profile }) => {
-  const fields = Object.keys(profile);
-  const grouped = groupBy(fields, field =>
-    RegistrationFields.getCategory(field)
-  );
-  const { education, portfolio, spokenLanguages, avatar } = profile;
-  return (
-    <React.Fragment>
-      {education && (
+    const fields = Object.keys(profile);
+    const { education } = profile;
+    const { portfolio } = profile.social;
+    const { spokenLanguages = [], profilePicture } = profile.profile;
+    return (
         <React.Fragment>
-          <Typography variant="h4">Education</Typography>
-          <Typography>{education.level}</Typography>
-        </React.Fragment>
-      )}
-      {portfolio && (
-        <React.Fragment>
-          <Typography variant="h4">Portfolio</Typography>
-          <Link>{portfolio}</Link>
-        </React.Fragment>
-      )}
+            {education && (
+                <React.Fragment>
+                    <Typography variant="h4">Education</Typography>
+                    <Typography>{education.level}</Typography>
+                </React.Fragment>
+            )}
+            {portfolio && (
+                <React.Fragment>
+                    <Typography variant="h4">Portfolio</Typography>
+                    <Link>{portfolio}</Link>
+                </React.Fragment>
+            )}
 
-      <Typography variant="h4">Interests</Typography>
-      <Link>{portfolio}</Link>
-      <ExpansionPanel key="languages">
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="languages-content"
-          id="languages-header"
-        >
-          <Typography>Languages</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          {spokenLanguages.map(language => (
-            <p>
-              <Typography>{language}</Typography>
-            </p>
-          ))}
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <textarea></textarea>
-    </React.Fragment>
-  );
+            <Typography variant="h4">Interests</Typography>
+            <Link>{portfolio}</Link>
+            <ExpansionPanel key="languages">
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="languages-content"
+                    id="languages-header"
+                >
+                    <Typography>Languages</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    {spokenLanguages.map(language => (
+                        <p>
+                            <Typography>{language}</Typography>
+                        </p>
+                    ))}
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <textarea></textarea>
+        </React.Fragment>
+    );
 });
 
 const mapState = state => ({});
