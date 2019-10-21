88379a24b0cb8b080d3ebf7495031b73d9dcdac3
petrus <holm.petrus@gmail.com>
2019-10-21 16:02:54 +0300

    Boilerplate for favorite feature

--- frontend/src/redux/recruitment/actionTypes.js
+++ frontend/src/redux/recruitment/actionTypes.js
@@ -1,1 +1,3 @@
 export const UPDATE_SEARCH_RESULTS = 'recruitment/UPDATE_SEARCH_RESULTS';
+
+export const SUBMIT_ACTION = 'recruitment/SUBMIT_ACTION';
--- frontend/src/services/recruitment.js
+++ frontend/src/services/recruitment.js
@@ -3,34 +3,57 @@ import _axios from 'services/axios';
 const RecruitmentService = {};
 
 function config(idToken) {
-    return {
-        headers: {
-            Authorization: `Bearer ${idToken}`
-        }
-    };
+  return {
+    headers: {
+      Authorization: `Bearer ${idToken}`
+    }
+  };
 }
 
 RecruitmentService.search = idToken => {
-    return _axios.post(
-        '/recruitment/search',
+  return _axios.post(
+    '/recruitment/search',
+    {
+      filters: [
         {
-            filters: [
-                {
-                    field: 'roles.role',
-                    operator: '==',
-                    value: 'Fullstack Developer'
-                }
-            ],
-            pagination: {
-                page_size: 25,
-                page: 0
-            }
-        },
-        config(idToken)
-    );
+          field: 'roles.role',
+          operator: '==',
+          value: 'Fullstack Developer'
+        }
+      ],
+      pagination: {
+        page_size: 25,
+        page: 0
+      }
+    },
+    config(idToken)
+  );
 };
 RecruitmentService.getUserProfile = (idToken, userId) => {
-    return _axios.get(`/recruitment/profile/${userId}`, config(idToken));
+  return _axios.get(`/recruitment/profile/${userId}`, config(idToken));
+};
+
+RecruitmentService.submitAction = (
+  type,
+  idToken,
+  recruiter,
+  userId,
+  organization,
+  message
+) => {
+  return _axios.post(
+    '/recruitment/action',
+    {
+      action: {
+        type,
+        recruiter,
+        organization,
+        userId,
+        data: { message: message }
+      }
+    },
+    config(idToken)
+  );
 };
 
 export default RecruitmentService;
