511b5e36dc8631cf9c10cfa1d7ed1c117ee93dd5
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-21 14:41:53 +0300

    Recruitment page layout changes

--- frontend/src/pages/Recruitment/Search/SearchBox.js
+++ frontend/src/pages/Recruitment/Search/SearchBox.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-import { Paper, Box, Button, CircularProgress } from '@material-ui/core';
+import { Paper, Box, Grid, Button, CircularProgress, Typography, Divider } from '@material-ui/core';
 import * as RecruitmentActions from 'redux/recruitment/actions';
 import * as RecruitmentSelectors from 'redux/recruitment/selectors';
 
 const SearchBox = ({ loading, updateSearchResults }) => {
     return (
         <Paper>
+            <Box p={3}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Typography variant="h6">Search participants</Typography>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Divider />
+                    </Grid>
+                </Grid>
+            </Box>
             <Box p={3} display="flex" flexDirection="row" alignItems="center" justifyContent="center">
                 {loading && <CircularProgress size={24} />}
                 {!loading && (
--- frontend/src/pages/Recruitment/Search/index.js
+++ frontend/src/pages/Recruitment/Search/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
 
-import PageWrapper from 'components/PageWrapper';
-import SearchBox from './SearchBox';
+import { makeStyles } from '@material-ui/core/styles';
+import { Drawer } from '@material-ui/core';
+
 import SearchResults from './SearchResults';
+import SearchBox from './SearchBox';
+
+const DRAWER_WIDTH = 500;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flex: 1
+    },
+    drawer: {
+        width: DRAWER_WIDTH,
+        flexShrink: 0,
+        backgroundColor: 'white'
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3)
+    }
+}));
 
 const SearchPage = () => {
+    const classes = useStyles();
     return (
-        <PageWrapper>
-            <SearchBox />
-            <SearchResults />
-        </PageWrapper>
+        <div className={classes.root}>
+            <Drawer className={classes.drawer} variant="permanent">
+                <SearchBox />
+            </Drawer>
+            <main className={classes.content}>
+                <SearchResults />
+            </main>
+        </div>
+        // <PageWrapper>
+        //     <SearchBox />
+        //     <SearchResults />
+        // </PageWrapper>
     );
 };
 
