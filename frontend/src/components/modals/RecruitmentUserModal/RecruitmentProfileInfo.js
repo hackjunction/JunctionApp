12f661ed9c69f85990e4816fa386a4b4bf64f4ac
petrus <holm.petrus@gmail.com>
2019-10-25 17:31:11 +0300

    Fixed favorite feature

--- frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
+++ frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
@@ -4,93 +4,84 @@ import { Typography, Link, Grid, Box } from '@material-ui/core';
 import Button from 'components/generic/Button';
 import { Input } from 'antd';
 
+import * as RecruitmentActions from 'redux/recruitment/actions';
+
 const getListOf = (areas, subject) => {
-    if (areas && areas.length !== 0)
-        return (
-            <Box mb={1} sm={12} md={6} lg={6}>
-                {subject === 'theme' ? (
-                    <Typography variant="h6">Themes of interest</Typography>
-                ) : (
-                    <Typography variant="h6">Industries of interest</Typography>
-                )}
-                {areas.map(area => {
-                    return <Typography key={area}>{area}</Typography>;
-                })}
-            </Box>
-        );
+  if (areas && areas.length !== 0)
+    return (
+      <Box mb={1} sm={12} md={6} lg={6}>
+        {subject === 'theme' ? (
+          <Typography variant="h6">Themes of interest</Typography>
+        ) : (
+          <Typography variant="h6">Industries of interest</Typography>
+        )}
+        {areas.map(area => {
+          return <Typography key={area}>{area}</Typography>;
+        })}
+      </Box>
+    );
 };
 
 const getActionHistory = history => {
-    if (history && history.length !== 0)
-        return (
-            <Grid item mb={1} sm={12} md={6} lg={6}>
-                <Typography variant="subtitle1">Previous messages</Typography>
-                {history.map(action => {
-                    return <Typography key={action}>{action}</Typography>;
-                })}
-            </Grid>
-        );
+  if (history && history.length !== 0)
+    return (
+      <Grid item mb={1} sm={12} md={6} lg={6}>
+        <Typography variant="subtitle1">Previous messages</Typography>
+        {history.map(action => {
+          return <Typography key={action}>{action}</Typography>;
+        })}
+      </Grid>
+    );
 };
 
 const getPrevEvents = events => {
-    if (events && events.length !== 0)
-        return (
-            <Grid item mb={1} sm={12} md={6} lg={6}>
-                <Typography variant="h6">Past hackathons</Typography>
-                {events.map(event => {
-                    return <Typography key={event.id}>{event.name}</Typography>;
-                })}
-            </Grid>
-        );
+  if (events && events.length !== 0)
+    return (
+      <Grid item mb={1} sm={12} md={6} lg={6}>
+        <Typography variant="h6">Past hackathons</Typography>
+        {events.map(event => {
+          return <Typography key={event.id}>{event.name}</Typography>;
+        })}
+      </Grid>
+    );
 };
 
-// const handleSendClick = () => {};
-
-const RecruitmentProfileInfo = React.memo(({ participant }) => {
-    const {
-        // education,
-        themesOfInterest,
-        industriesOfInterest,
-        previousEvents,
-        social
-    } = participant;
-    const {
-        // spokenLanguages = [],
-        // profilePicture,
-        recruitmentActionHistory,
-        firstName
-    } = participant.profile;
-    return (
-        <Grid container>
-            <Grid container direction="column" justify="space-between">
-                {social && social.length !== 0 && (
-                    <Grid item mb={2} sm={12} md={6} lg={6}>
-                        <Typography variant="h6">Social stuff</Typography>
-                        {Object.keys(social).map(service => {
-                            return <Link>{social[service]}</Link>;
-                        })}
-                    </Grid>
-                )}
-                {getListOf(themesOfInterest, 'theme')}
-                {getListOf(industriesOfInterest, 'industry')}
-                {getPrevEvents(previousEvents)}
-            </Grid>
-            <Grid item sm={12} md={8} lg={6}>
-                <Typography variant="h6">Contact</Typography>
-                {getActionHistory(recruitmentActionHistory)}
-                <Typography>Send {firstName} a message</Typography>
-                <Input.TextArea autosize={{ minRows: 10, maxRows: 20 }} placeholder="Max 1000 characters" />
-                <Button block text="Send" button={{ onClick: () => this.handleSendCLick }} />
-            </Grid>
-        </Grid>
-    );
+const RecruitmentProfileInfo = React.memo(({ participant, sendMessage }) => {
+  const {
+    // education,
+    themesOfInterest,
+    industriesOfInterest,
+    previousEvents,
+    social
+  } = participant;
+  const { recruitmentActionHistory, firstName } = participant.profile;
+  return (
+    <Grid container>
+      <Grid container direction="column" justify="space-between">
+        {social && social.length !== 0 && (
+          <Grid item mb={2} sm={12} md={6} lg={6}>
+            <Typography variant="h6">Social stuff</Typography>
+            {Object.keys(social).map(service => {
+              return <Link>{social[service]}</Link>;
+            })}
+          </Grid>
+        )}
+        {getListOf(themesOfInterest, 'theme')}
+        {getListOf(industriesOfInterest, 'industry')}
+        {getPrevEvents(previousEvents)}
+      </Grid>
+      <Grid item sm={12} md={8} lg={6}>
+        <Typography variant="h6">Contact</Typography>
+        {getActionHistory(recruitmentActionHistory)}
+        <Typography>Send {firstName} a message</Typography>
+        <Input.TextArea
+          autosize={{ minRows: 10, maxRows: 20 }}
+          placeholder="Max 1000 characters"
+        />
+        <Button block text="Send" button={{ onClick: () => sendMessage() }} />
+      </Grid>
+    </Grid>
+  );
 });
 
-const mapState = state => ({});
-
-const mapDispatch = dispatch => ({});
-
-export default connect(
-    mapState,
-    mapDispatch
-)(RecruitmentProfileInfo);
+export default RecruitmentProfileInfo;
--- frontend/src/components/modals/RecruitmentUserModal/index.js
+++ frontend/src/components/modals/RecruitmentUserModal/index.js
@@ -17,92 +17,120 @@ import * as AuthSelectors from 'redux/auth/selectors';
 
 import UserProfilesService from 'services/userProfiles';
 
-import { toggleFavorite } from 'redux/recruitment/actions';
+import * as RecruitmentActions from 'redux/recruitment/actions';
 
-const RecruitmentUserModal = ({ idToken, profileId, onClose, event }) => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [participant, setParticipant] = useState();
+const RecruitmentUserModal = ({
+  idToken,
+  profileId,
+  onClose,
+  toggleFavorite,
+  sendMessage
+}) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [participant, setParticipant] = useState();
 
-    useEffect(() => {
-        console.log('id:', profileId);
-        if (profileId) {
-            setLoading(true);
-            UserProfilesService.getUserProfileRecruitment(profileId, idToken)
-                .then(data => {
-                    setParticipant(data);
-                    console.log(data);
-                })
-                .catch(err => {
-                    setError(true);
-                })
-                .finally(() => {
-                    setLoading(false);
-                });
-        }
-    }, [idToken, profileId]);
+  useEffect(() => {
+    console.log('id:', profileId);
+    if (profileId) {
+      setLoading(true);
+      UserProfilesService.getUserProfileRecruitment(profileId, idToken)
+        .then(data => {
+          setParticipant(data);
+          console.log(data);
+        })
+        .catch(err => {
+          setError(true);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [idToken, profileId]);
 
-    const participantName = useMemo(() => {
-        if (!participant) return '';
-        const { firstName, lastName } = participant.profile;
-        return `${firstName} ${lastName}`;
-    }, [participant]);
+  const participantName = useMemo(() => {
+    if (!participant) return '';
+    const { firstName, lastName } = participant.profile;
+    return `${firstName} ${lastName}`;
+  }, [participant]);
 
-    const participantSubheading = useMemo(() => {
-        if (!participant) return '';
-        return participant.profile.countryOfResidence;
-    }, [participant]);
+  const participantSubheading = useMemo(() => {
+    if (!participant) return '';
+    return participant.profile.countryOfResidence;
+  }, [participant]);
 
-    const participantImageUrl = useMemo(() => {
-        if (!participant) return '';
-        return participant.profile.avatar;
-    }, [participant]);
+  const participantImageUrl = useMemo(() => {
+    if (!participant) return '';
+    return participant.profile.avatar;
+  }, [participant]);
 
-    const { education, roles } = participant || {};
+  const { education, roles } = participant || {};
 
-    return (
-        <Modal isOpen={!!profileId} handleClose={onClose} size="max" title="Profile details">
-            <PageWrapper loading={loading || !participant} error={error}>
-                <Button block text="Add to favorites" button={{ onClick: () => toggleFavorite(profileId, idToken) }} />
-                <Grid container direction="row" justify="space-around">
-                    <Grid item sm={8} md={8} lg={8}>
-                        <Typography variant="h3">{participantName}</Typography>
-                        <Typography variant="subtitle1">{participantSubheading}</Typography>
-                        {education && education.level && (
-                            <Grid item mb={1}>
-                                <Typography variant="h6">Education</Typography>
-                                <Typography>
-                                    {education.level} in {education.degree}, {education.university} (
-                                    {education.graduationYear})
-                                </Typography>
-                            </Grid>
-                        )}
-                        {roles && roles.length !== 0 && (
-                            <Grid item mb={1}>
-                                <Typography variant="h6">Previous roles</Typography>
-                                {roles.map(a => {
-                                    return <Typography>{a.role}</Typography>;
-                                })}
-                            </Grid>
-                        )}
-                    </Grid>
-                    <Grid item sm={4} md={4} lg={4}>
-                        <Image
-                            url={participantImageUrl}
-                            defaultImage="https://avatars1.githubusercontent.com/u/11797156?s=460&v=4"
-                            alt="Profile picture"
-                            className={styles.profilePic}
-                        />
-                    </Grid>
-                </Grid>
-                <RecruitmentProfileInfo participant={participant} />
-            </PageWrapper>
-        </Modal>
-    );
+  return (
+    <Modal
+      isOpen={!!profileId}
+      handleClose={onClose}
+      size="max"
+      title="Profile details"
+    >
+      <PageWrapper loading={loading || !participant} error={error}>
+        <Button
+          block
+          text="Add to favorites"
+          button={{ onClick: e => toggleFavorite(profileId, idToken) }}
+        />
+        <Grid container direction="row" justify="space-around">
+          <Grid item sm={8} md={8} lg={8}>
+            <Typography variant="h3">{participantName}</Typography>
+            <Typography variant="subtitle1">{participantSubheading}</Typography>
+            {education && education.level && (
+              <Grid item mb={1}>
+                <Typography variant="h6">Education</Typography>
+                <Typography>
+                  {education.level} in {education.degree},{' '}
+                  {education.university} ({education.graduationYear})
+                </Typography>
+              </Grid>
+            )}
+            {roles && roles.length !== 0 && (
+              <Grid item mb={1}>
+                <Typography variant="h6">Previous roles</Typography>
+                {roles.map(a => {
+                  return <Typography>{a.role}</Typography>;
+                })}
+              </Grid>
+            )}
+          </Grid>
+          <Grid item sm={4} md={4} lg={4}>
+            <Image
+              url={participantImageUrl}
+              defaultImage="https://avatars1.githubusercontent.com/u/11797156?s=460&v=4"
+              alt="Profile picture"
+              className={styles.profilePic}
+            />
+          </Grid>
+        </Grid>
+        <RecruitmentProfileInfo
+          participant={participant}
+          sendMessage={sendMessage}
+        />
+      </PageWrapper>
+    </Modal>
+  );
 };
 
 const mapState = state => ({
-    idToken: AuthSelectors.getIdToken(state)
+  idToken: AuthSelectors.getIdToken(state)
 });
 
-export default withSnackbar(connect(mapState)(RecruitmentUserModal));
+const mapDispatch = dispatch => ({
+  toggleFavorite: () => dispatch(RecruitmentActions.toggleFavorite()),
+  sendMessage: () => dispatch(RecruitmentActions.sendMessage())
+});
+
+export default withSnackbar(
+  connect(
+    mapState,
+    mapDispatch
+  )(RecruitmentUserModal)
+);
