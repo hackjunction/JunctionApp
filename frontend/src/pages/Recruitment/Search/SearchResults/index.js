97361e863510aa98d7d4cff171b8e8ccad6b9602
petrus <holm.petrus@gmail.com>
2019-10-25 17:31:11 +0300

    Fixed favorite feature

--- frontend/src/pages/Recruitment/Search/SearchResults/index.js
+++ frontend/src/pages/Recruitment/Search/SearchResults/index.js
@@ -12,78 +12,91 @@ import Pagination from './Pagination';
 import LoadingCard from './LoadingCard';
 
 const SearchResults = ({
-    searchResults,
-    searchResultsCount,
-    updateSearchResults,
-    filters,
-    pageSize,
-    page,
-    pageCount,
-    setNextPage,
-    setPrevPage,
-    loading
+  searchResults,
+  updateSearchResults,
+  filters,
+  pageSize,
+  page,
+  pageCount,
+  setNextPage,
+  setPrevPage,
+  loading
 }) => {
-    const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState();
 
-    useEffect(() => {
-        updateSearchResults();
-    }, [pageSize, page, filters, updateSearchResults]); //eslint-disable-line
+  useEffect(() => {
+    updateSearchResults();
+  }, [pageSize, page, filters, updateSearchResults]); //eslint-disable-line
 
-    return (
+  return (
+    <React.Fragment>
+      <Pagination />
+      {loading ? (
         <React.Fragment>
-            <Pagination />
-            {loading ? (
-                <React.Fragment>
-                    <LoadingCard />
-                    <LoadingCard />
-                    <LoadingCard />
-                    <LoadingCard />
-                    <LoadingCard />
-                </React.Fragment>
-            ) : (
-                <React.Fragment>
-                    {searchResults.map(item => (
-                        <ResultCard key={`item-${item.userId}`} data={item} onClick={e => setSelected(item.userId)} />
-                    ))}
-                </React.Fragment>
-            )}
-            {pageCount > 0 && (
-                <Box mt={2} display="flex" flexDirection="row" justifyContent="space-between">
-                    <Button disabled={loading || page === 0} color="primary" variant="contained" onClick={setPrevPage}>
-                        Previous page
-                    </Button>
-                    <Button
-                        disabled={loading || page + 1 === pageCount}
-                        color="primary"
-                        variant="contained"
-                        onClick={setNextPage}
-                    >
-                        Next page
-                    </Button>
-                </Box>
-            )}
-            <RecruitmentUserModal profileId={selected} onClose={setSelected} />
+          <LoadingCard />
+          <LoadingCard />
+          <LoadingCard />
+          <LoadingCard />
+          <LoadingCard />
         </React.Fragment>
-    );
+      ) : (
+        <React.Fragment>
+          {searchResults.map(item => (
+            <ResultCard
+              key={`item-${item.userId}`}
+              data={item}
+              onClick={e => setSelected(item.userId)}
+            />
+          ))}
+        </React.Fragment>
+      )}
+      {pageCount > 0 && (
+        <Box
+          mt={2}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+        >
+          <Button
+            disabled={loading || page === 0}
+            color="primary"
+            variant="contained"
+            onClick={setPrevPage}
+          >
+            Previous page
+          </Button>
+          <Button
+            disabled={loading || page + 1 === pageCount}
+            color="primary"
+            variant="contained"
+            onClick={setNextPage}
+          >
+            Next page
+          </Button>
+        </Box>
+      )}
+      <RecruitmentUserModal profileId={selected} onClose={setSelected} />
+    </React.Fragment>
+  );
 };
 
 const mapState = state => ({
-    searchResults: RecruitmentSelectors.searchResults(state),
-    searchResultsCount: RecruitmentSelectors.searchResultsCount(state),
-    loading: RecruitmentSelectors.searchResultsLoading(state),
-    filters: RecruitmentSelectors.filters(state),
-    pageSize: RecruitmentSelectors.pageSize(state),
-    page: RecruitmentSelectors.page(state),
-    pageCount: RecruitmentSelectors.pageCount(state)
+  searchResults: RecruitmentSelectors.searchResults(state),
+  searchResultsCount: RecruitmentSelectors.searchResultsCount(state),
+  loading: RecruitmentSelectors.searchResultsLoading(state),
+  filters: RecruitmentSelectors.filters(state),
+  pageSize: RecruitmentSelectors.pageSize(state),
+  page: RecruitmentSelectors.page(state),
+  pageCount: RecruitmentSelectors.pageCount(state)
 });
 
 const mapDispatch = dispatch => ({
-    updateSearchResults: () => dispatch(RecruitmentActions.updateSearchResults()),
-    setPrevPage: () => dispatch(RecruitmentActions.setPrevPage()),
-    setNextPage: () => dispatch(RecruitmentActions.setNextPage())
+  updateSearchResults: () => dispatch(RecruitmentActions.updateSearchResults()),
+  setPrevPage: () => dispatch(RecruitmentActions.setPrevPage()),
+  setNextPage: () => dispatch(RecruitmentActions.setNextPage())
 });
 
 export default connect(
-    mapState,
-    mapDispatch
+  mapState,
+  mapDispatch
 )(SearchResults);
--- frontend/src/redux/recruitment/actions.js
+++ frontend/src/redux/recruitment/actions.js
@@ -6,66 +6,79 @@ import { buildFilterArray } from './helpers';
 import RecruitmentService from 'services/recruitment';
 
 export const setFilters = data => dispatch => {
-    dispatch({
-        type: ActionTypes.SET_FILTERS,
-        payload: data
-    });
+  dispatch({
+    type: ActionTypes.SET_FILTERS,
+    payload: data
+  });
 };
 
 export const setPageSize = size => ({
-    type: ActionTypes.SET_PAGE_SIZE,
-    payload: size
+  type: ActionTypes.SET_PAGE_SIZE,
+  payload: size
 });
 
 export const setPage = page => ({
-    type: ActionTypes.SET_PAGE,
-    payload: page
+  type: ActionTypes.SET_PAGE,
+  payload: page
 });
 
 export const setPrevPage = () => ({
-    type: ActionTypes.SET_PREV_PAGE
+  type: ActionTypes.SET_PREV_PAGE
 });
 
 export const setNextPage = () => ({
-    type: ActionTypes.SET_NEXT_PAGE
+  type: ActionTypes.SET_NEXT_PAGE
 });
 
 export const updateSearchResults = () => (dispatch, getState) => {
-    const state = getState();
-    const idToken = AuthSelectors.getIdToken(state);
-    const page = RecruitmentSelectors.page(state);
-    const pageSize = RecruitmentSelectors.pageSize(state);
-    const filters = buildFilterArray(RecruitmentSelectors.filters(state));
+  const state = getState();
+  const idToken = AuthSelectors.getIdToken(state);
+  const page = RecruitmentSelectors.page(state);
+  const pageSize = RecruitmentSelectors.pageSize(state);
+  const filters = buildFilterArray(RecruitmentSelectors.filters(state));
 
-    dispatch({
-        type: ActionTypes.UPDATE_SEARCH_RESULTS,
-        promise: RecruitmentService.search(idToken, filters, page, pageSize),
-        meta: {
-            onFailure: e => console.log('Error getting search results', e)
-        }
-    });
+  dispatch({
+    type: ActionTypes.UPDATE_SEARCH_RESULTS,
+    promise: RecruitmentService.search(idToken, filters, page, pageSize),
+    meta: {
+      onFailure: e => console.log('Error getting search results', e)
+    }
+  });
 };
 
-export const sendMessage = (organization, message, recruiter, userId) => (dispatch, getState) => {
-    const idToken = AuthSelectors.getIdToken(getState());
-
-    dispatch({
-        type: ActionTypes.SUBMIT_ACTION,
-        promise: RecruitmentService.submitAction('message', idToken, recruiter, userId, organization, message),
-        meta: {
-            onFailure: e => console.log('Error sending message', e)
-        }
-    });
+export const sendMessage = (organization, message, recruiter, userId) => (getState, dispatch) => {
+  const state = getState();
+  const idToken = AuthSelectors.getIdToken(state);
+  dispatch({
+    type: ActionTypes.SUBMIT_ACTION,
+    promise: RecruitmentService.submitAction(
+      'message',
+      idToken,
+      recruiter,
+      userId,
+      organization,
+      message
+    ),
+    meta: {
+      onFailure: e => console.log('Error sending message', e)
+    }
+  });
 };
 
 export const toggleFavorite = (organization, recruiter, userId) => (dispatch, getState) => {
-    const idToken = AuthSelectors.getIdToken(getState());
-
-    dispatch({
-        type: ActionTypes.SUBMIT_ACTION,
-        promise: RecruitmentService.submitAction('favorite', idToken, recruiter, userId, organization),
-        meta: {
-            onFailure: e => console.log('Error adding to favorites', e)
-        }
-    });
+  const idToken = AuthSelectors.getIdToken(getState());
+  console.log('called toggle');
+  dispatch({
+    type: ActionTypes.SUBMIT_ACTION,
+    promise: RecruitmentService.submitAction(
+      'favorite',
+      idToken,
+      recruiter,
+      userId,
+      organization
+    ),
+    meta: {
+      onFailure: e => console.log('Error adding to favorites', e)
+    }
+  });
 };
