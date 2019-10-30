8093317f4d140b64ef6328971dc4c0c091bbfa89
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-30 17:22:21 +0200

    Update event model with new configuration fields

--- frontend/src/components/inputs/StreetAddressForm/index.js
+++ frontend/src/components/inputs/StreetAddressForm/index.js
@@ -1,37 +1,63 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 
 import { Grid } from '@material-ui/core';
 import Select from 'components/inputs/Select';
 import TextInput from 'components/inputs/TextInput';
 
-const StreetAddressInput = ({ showVenueName }) => {
-    const [country, setCountry] = useState();
-    const [city, setCity] = useState();
-    const [address, setAddress] = useState();
-    const [address2, setAddress2] = useState();
-    const [postalCode, setPostalCode] = useState();
-    const [venueName, setVenueName] = useState();
+const StreetAddressInput = ({ value = {}, onChange, showVenueName = false }) => {
+    const { country, addressLine, addressLine2, city, postalCode, venueName } = value;
+
+    const handleChange = useCallback(
+        (fieldName, fieldValue) => {
+            onChange({
+                ...value,
+                [fieldName]: fieldValue
+            });
+        },
+        [value, onChange]
+    );
 
     return (
         <Grid container spacing={3}>
             <Grid item xs={12}>
-                <Select label="Country" options="country" value={country} onChange={setCountry} />
+                <Select
+                    label="Country"
+                    options="country"
+                    value={country}
+                    onChange={value => handleChange('country', value)}
+                />
             </Grid>
             <Grid item xs={9}>
-                <TextInput label="Street address" value={address} onChange={setAddress} />
+                <TextInput
+                    label="Street address"
+                    value={addressLine}
+                    onChange={value => handleChange('addressLine', value)}
+                />
             </Grid>
             <Grid item xs={3}>
-                <TextInput label="Apartment/Unit/Other" value={address2} onChange={setAddress2} />
+                <TextInput
+                    label="Apartment/Unit/Other"
+                    value={addressLine2}
+                    onChange={value => handleChange('addressLine2', value)}
+                />
             </Grid>
             <Grid item xs={8}>
-                <TextInput label="City" value={city} onChange={setCity} />
+                <TextInput label="City" value={city} onChange={value => handleChange('city', value)} />
             </Grid>
             <Grid item xs={4}>
-                <TextInput label="Postal code" value={postalCode} onChange={setPostalCode} />
+                <TextInput
+                    label="Postal code"
+                    value={postalCode}
+                    onChange={value => handleChange('postalCode', value)}
+                />
             </Grid>
             {showVenueName && (
                 <Grid item xs={12}>
-                    <TextInput label="Venue name" value={venueName} onChange={setVenueName} />
+                    <TextInput
+                        label="Venue name"
+                        value={venueName}
+                        onChange={value => handleChange('venueName', value)}
+                    />
                 </Grid>
             )}
         </Grid>
