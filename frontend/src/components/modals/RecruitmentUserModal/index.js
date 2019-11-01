22f09fa309d4fa6ed3b92dd1139cf001318b78a9
Verneri Heikkila <verneri.heikkila@tuta.io>
2019-11-01 17:51:19 +0200

    RecruitmentUserModal Image changed to Avatar component & default image added

--- frontend/src/components/modals/RecruitmentUserModal/index.js
+++ frontend/src/components/modals/RecruitmentUserModal/index.js
@@ -5,7 +5,7 @@ import Image from 'components/generic/Image';
 import Button from 'components/generic/Button';
 
 import { withSnackbar } from 'notistack';
-import { Typography, Grid, Icon, Link, Box } from '@material-ui/core';
+import { Typography, Grid, Icon, Link, Box, Avatar } from '@material-ui/core';
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -17,6 +17,8 @@ import LinkBall from './LinkBall';
 
 import PageWrapper from 'components/layouts/PageWrapper';
 
+import emblem_black from '../../../assets/logos/emblem_black.png';
+
 import RecruitmentProfileInfo from './RecruitmentProfileInfo';
 
 import CenteredContainer from 'components/generic/CenteredContainer';
@@ -27,153 +29,165 @@ import * as RecruitmentSelectors from 'redux/recruitment/selectors';
 import UserProfilesService from 'services/userProfiles';
 
 import * as RecruitmentActions from 'redux/recruitment/actions';
+import { height } from '@material-ui/system';
 
 const RecruitmentUserModal = ({
-  idToken,
-  profileId,
-  onClose,
-  isFavorite,
-  toggleFavorite
+    idToken,
+    profileId,
+    onClose,
+    isFavorite,
+    toggleFavorite
 }) => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [participant, setParticipant] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [participant, setParticipant] = useState();
 
-  useEffect(() => {
-    if (profileId) {
-      setLoading(true);
+    useEffect(() => {
+        if (profileId) {
+            setLoading(true);
 
-      UserProfilesService.getUserProfileRecruitment(profileId, idToken)
-        .then(data => {
-          setParticipant(data);
-          console.log(data);
-        })
-        .catch(err => {
-          setError(true);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
-  }, [idToken, profileId]);
+            UserProfilesService.getUserProfileRecruitment(profileId, idToken)
+                .then(data => {
+                    setParticipant(data);
+                    console.log(data);
+                })
+                .catch(err => {
+                    setError(true);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
+    }, [idToken, profileId]);
 
-  const participantName = useMemo(() => {
-    if (!participant) return '';
-    const { firstName, lastName } = participant.profile;
-    return `${firstName} ${lastName}`;
-  }, [participant]);
+    const participantName = useMemo(() => {
+        if (!participant) return '';
+        const { firstName, lastName } = participant.profile;
+        return `${firstName} ${lastName}`;
+    }, [participant]);
 
-  const participantSubheading = useMemo(() => {
-    if (!participant) return '';
-    return participant.profile.countryOfResidence;
-  }, [participant]);
+    const participantSubheading = useMemo(() => {
+        if (!participant) return '';
+        return participant.profile.countryOfResidence;
+    }, [participant]);
 
-  const participantImageUrl = useMemo(() => {
-    if (!participant) return '';
-    return participant.profile.profilePicture;
-  }, [participant]);
+    const participantImageUrl = useMemo(() => {
+        if (!participant) return '';
+        return participant.profile.profilePicture;
+    }, [participant]);
 
-  const { social } = participant || {};
+    const { social } = participant || {};
 
-  const renderStar = () => {
-    if (isFavorite) {
-      return (
-        <StarIcon
-          fontSize="large"
-          onClick={() => toggleFavorite(profileId, isFavorite)}
-        />
-      );
-    }
+    const renderStar = () => {
+        if (isFavorite) {
+            return (
+                <StarIcon
+                    fontSize="large"
+                    onClick={() => toggleFavorite(profileId, isFavorite)}
+                />
+            );
+        }
+        return (
+            <StarBorderIcon
+                fontSize="large"
+                onClick={() => toggleFavorite(profileId, isFavorite)}
+            />
+        );
+    };
     return (
-      <StarBorderIcon
-        fontSize="large"
-        onClick={() => toggleFavorite(profileId, isFavorite)}
-      />
+        <Modal
+            isOpen={!!profileId}
+            handleClose={onClose}
+            size="max"
+            title="Profile details"
+        >
+            <PageWrapper
+                loading={loading || !participant}
+                error={error}
+                render={() => (
+                    <CenteredContainer>
+                        <Box alignItems="center" justifyContent="center">
+                            <Grid
+                                container
+                                direction="column"
+                                alignItems="center"
+                                justifyContent="center"
+                            >
+                                <Grid item>{renderStar()}</Grid>
+                                <Grid item>
+                                    {/* Image .scss rules still exist */}
+                                    <Avatar
+                                        style={{ height: '300px', width: '300px' }}
+                                        alt="Profile Picture"
+                                        src={participantImageUrl}
+                                        imgProps={{
+                                            onError: e => {
+                                                e.target.src = emblem_black;
+                                            }
+                                        }}
+                                    />
+                                    {/* <Image
+                                        url={participantImageUrl}
+                                        alt="Profile picture"
+                                        className={styles.profilePic}
+                                    /> */}
+                                </Grid>
+                                <Grid
+                                    item
+                                    alignItems="center"
+                                    justifyContent="center"
+                                    className={styles.nameContainer}
+                                >
+                                    <Typography variant="h3">{participantName}</Typography>
+                                    <Typography variant="h6">{participantSubheading}</Typography>
+                                </Grid>
+                                <Grid item>
+                                    {social && social.linkedin && (
+                                        <LinkBall target={social.linkedin}>
+                                            <LinkedInIcon />
+                                        </LinkBall>
+                                    )}
+                                    {social && social.github && (
+                                        <LinkBall target={social.github}>
+                                            <GitHubIcon />
+                                        </LinkBall>
+                                    )}
+                                </Grid>
+                            </Grid>
+                            <RecruitmentProfileInfo participant={participant} />
+                        </Box>
+                    </CenteredContainer>
+                )}
+            />
+        </Modal>
     );
-  };
-  return (
-    <Modal
-      isOpen={!!profileId}
-      handleClose={onClose}
-      size="max"
-      title="Profile details"
-    >
-      <PageWrapper
-        loading={loading || !participant}
-        error={error}
-        render={() => (
-          <CenteredContainer>
-            <Box alignItems="center" justifyContent="center">
-              <Grid
-                container
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Grid item>{renderStar()}</Grid>
-                <Grid item>
-                  <Image
-                    url={participantImageUrl}
-                    alt="Profile picture"
-                    className={styles.profilePic}
-                  />
-                </Grid>
-                <Grid
-                  item
-                  alignItems="center"
-                  justifyContent="center"
-                  className={styles.nameContainer}
-                >
-                  <Typography variant="h3">{participantName}</Typography>
-                  <Typography variant="h6">{participantSubheading}</Typography>
-                </Grid>
-                <Grid item>
-                  {social && social.linkedin && (
-                    <LinkBall target={social.linkedin}>
-                      <LinkedInIcon />
-                    </LinkBall>
-                  )}
-                  {social && social.github && (
-                    <LinkBall target={social.github}>
-                      <GitHubIcon />
-                    </LinkBall>
-                  )}
-                </Grid>
-              </Grid>
-              <RecruitmentProfileInfo participant={participant} />
-            </Box>
-          </CenteredContainer>
-        )}
-      />
-    </Modal>
-  );
 };
 
 const mapState = (state, ownProps) => {
-  const actionHistory = RecruitmentSelectors.actionHistory(state);
-  const filteredActions = actionHistory.filter(
-    action => action.user === ownProps.profileId
-  );
-  const isFavorite =
-    actionHistory.filter(
-      action => action.user === ownProps.profileId && action.type === 'favorite'
-    ).length !== 0;
+    const actionHistory = RecruitmentSelectors.actionHistory(state);
+    const filteredActions = actionHistory.filter(
+        action => action.user === ownProps.profileId
+    );
+    const isFavorite =
+        actionHistory.filter(
+            action => action.user === ownProps.profileId && action.type === 'favorite'
+        ).length !== 0;
 
-  return {
-    idToken: AuthSelectors.getIdToken(state),
-    actions: filteredActions,
-    isFavorite
-  };
+    return {
+        idToken: AuthSelectors.getIdToken(state),
+        actions: filteredActions,
+        isFavorite
+    };
 };
 
 const mapDispatch = dispatch => ({
-  toggleFavorite: (profileId, isFavorite) =>
-    dispatch(RecruitmentActions.toggleFavorite(profileId, isFavorite))
+    toggleFavorite: (profileId, isFavorite) =>
+        dispatch(RecruitmentActions.toggleFavorite(profileId, isFavorite))
 });
 
 export default withSnackbar(
-  connect(
-    mapState,
-    mapDispatch
-  )(RecruitmentUserModal)
+    connect(
+        mapState,
+        mapDispatch
+    )(RecruitmentUserModal)
 );
