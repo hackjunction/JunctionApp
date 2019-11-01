fb9448c2e05a1b8a25d44c8dc22a48fc245f7ecc
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-11-01 17:36:40 +0200

    Fix message filtering in recruitment profile

--- frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
+++ frontend/src/components/modals/RecruitmentUserModal/RecruitmentProfileInfo.js
@@ -12,157 +12,135 @@ import styles from './RecruitmentUserModal.module.scss';
 import * as RecruitmentSelectors from 'redux/recruitment/selectors';
 
 const getListOf = (areas, subject) => {
-  if (areas && areas.length !== 0)
-    return (
-      <Grid item mb={1}>
-        {subject === 'theme' ? (
-          <Typography variant="h6">Themes of interest</Typography>
-        ) : (
-            <Typography variant="h6">Industries of interest</Typography>
-          )}
-        {areas.map(area => {
-          return (
-            <Typography key={area}>
-              <CheckCircleRoundedIcon
-                fontSize="inherit"
-                className={styles.listItem}
-              />
-              {area}
-            </Typography>
-          );
-        })}
-      </Grid>
-    );
+    if (areas && areas.length !== 0)
+        return (
+            <Grid item mb={1}>
+                {subject === 'theme' ? (
+                    <Typography variant="h6">Themes of interest</Typography>
+                ) : (
+                    <Typography variant="h6">Industries of interest</Typography>
+                )}
+                {areas.map(area => {
+                    return (
+                        <Typography key={area}>
+                            <CheckCircleRoundedIcon fontSize="inherit" className={styles.listItem} />
+                            {area}
+                        </Typography>
+                    );
+                })}
+            </Grid>
+        );
 };
 
 const getSkills = skills => {
-  if (skills && skills.length !== 0)
-    return (
-      <Grid item mb={1} xs={12} md={6} lg={6}>
-        <Typography variant="h6">Skills</Typography>
-        {skills.map(a => {
-          return <SkillRating data={a} key={a.skill} />;
-        })}
-      </Grid>
-    );
+    if (skills && skills.length !== 0)
+        return (
+            <Grid item mb={1} xs={12} md={6} lg={6}>
+                <Typography variant="h6">Skills</Typography>
+                {skills.map(a => {
+                    return <SkillRating data={a} key={a.skill} />;
+                })}
+            </Grid>
+        );
 };
 
 const getActionHistory = messages => {
-  if (messages && messages.length !== 0) {
-    return (
-      <Grid item mb={1}>
-        <List>
-          {messages.map(action => {
-            return <MessageItem action={action} />;
-          })}
-        </List>
-      </Grid>
-    );
-  }
+    if (messages && messages.length !== 0) {
+        return (
+            <Grid item mb={1}>
+                <List>
+                    {messages.map(action => {
+                        return <MessageItem action={action} />;
+                    })}
+                </List>
+            </Grid>
+        );
+    }
 };
 
-const RecruitmentProfileInfo = React.memo(
-  ({ participant, sendMessage, actionHistory }) => {
-    const {
-      themesOfInterest,
-      industriesOfInterest,
-      skills,
-      education,
-      roles
-    } = participant;
+const RecruitmentProfileInfo = React.memo(({ participant, sendMessage, messages }) => {
+    const { themesOfInterest, industriesOfInterest, skills, education, roles } = participant;
     const { firstName } = participant.profile;
 
     const [messageVal, changeMessageValue] = useState('');
 
     return (
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        justifyContent="center"
-        xs={12}
-      >
-        <Grid
-          container
-          item
-          direction="column"
-          wrap="nowrap"
-          justifyContent="center"
-          xs={12}
-          sm={4}
-          spacing={4}
-        >
-          {roles && roles.length !== 0 && (
-            <Grid item mb={1}>
-              <Typography variant="h6">Previous roles</Typography>
-              {roles.map(a => {
-                return (
-                  <React.Fragment>
-                    <Typography variant="subtitle1">{a.role}</Typography>
-                    <Typography>{a.years} years</Typography>
-                  </React.Fragment>
-                );
-              })}
+        <Grid container direction="row" justify="center" justifyContent="center" xs={12}>
+            <Grid container item direction="column" wrap="nowrap" justifyContent="center" xs={12} sm={4} spacing={4}>
+                {education && education.level && (
+                    <Grid item mb={1} xs={12} md={6} lg={6}>
+                        <Typography variant="h6">Education</Typography>
+                        <Typography>
+                            <strong>
+                                {education.level} in {education.degree}
+                            </strong>
+                        </Typography>
+                        <Typography>{education.university}</Typography>
+                        <Typography>{education.graduationYear}</Typography>
+                    </Grid>
+                )}
+                {getListOf(industriesOfInterest, 'industry')}
+                {getListOf(themesOfInterest, 'theme')}
             </Grid>
-          )}
-          {getListOf(industriesOfInterest, 'industry')}
-          {getListOf(themesOfInterest, 'theme')}
+            <Grid container item direction="column" xs={12} wrap="nowrap" sm={8}>
+                <Grid container item spacing={4}>
+                    {getSkills(skills)}
+                    {roles && roles.length !== 0 && (
+                        <Grid item mb={1}>
+                            <Typography variant="h6">Previous roles</Typography>
+                            {roles.map(a => {
+                                return (
+                                    <React.Fragment>
+                                        <Typography variant="subtitle1">{a.role}</Typography>
+                                        <Typography>{a.years} years</Typography>
+                                    </React.Fragment>
+                                );
+                            })}
+                        </Grid>
+                    )}
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6">Contact</Typography>
+                    {getActionHistory(messages)}
+                    <Typography>Here you can send {firstName} a message.</Typography>
+                    <Input.TextArea
+                        onChange={e => changeMessageValue(e.target.value)}
+                        value={messageVal}
+                        autosize={{ minRows: 10, maxRows: 20 }}
+                        placeholder="Type message here..."
+                    />
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => (sendMessage(messageVal, participant.userId), changeMessageValue(''))}
+                        startIcon={<EmailIcon />}
+                    >
+                        Send
+                    </Button>
+                </Grid>
+            </Grid>
         </Grid>
-        <Grid container item direction="column" xs={12} wrap="nowrap" sm={8}>
-          <Grid container item spacing={4}>
-            {getSkills(skills)}
-            {education && education.level && (
-              <Grid item mb={1} xs={12} md={6} lg={6}>
-                <Typography variant="h6">Education</Typography>
-                <Typography>
-                  <strong>
-                    {education.level} in {education.degree}
-                  </strong>
-                </Typography>
-                <Typography>{education.university}</Typography>
-                <Typography>{education.graduationYear}</Typography>
-              </Grid>
-            )}
-          </Grid>
-          <Grid item>
-            <Typography variant="h6">Contact</Typography>
-            {getActionHistory(
-              actionHistory.filter(action => action.type === 'message')
-            )}
-            <Typography>Here you can send {firstName} a message.</Typography>
-            <Input.TextArea
-              onChange={e => changeMessageValue(e.target.value)}
-              value={messageVal}
-              autosize={{ minRows: 10, maxRows: 20 }}
-              placeholder="Type message here..."
-            />
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => (
-                sendMessage(messageVal, participant.userId),
-                changeMessageValue('')
-              )}
-              startIcon={<EmailIcon />}
-            >
-              Send
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
     );
-  }
-);
-
-const mapState = state => ({
-  actionHistory: RecruitmentSelectors.actionHistory(state)
 });
 
+const mapState = (state, ownProps) => {
+    const actionHistory = RecruitmentSelectors.actionHistory(state);
+
+    console.log('ACTIONS', actionHistory);
+
+    const messages = actionHistory.filter(
+        action => action.type === 'message' && action.user === ownProps.participant.userId
+    );
+    return {
+        messages
+    };
+};
+
 const mapDispatch = dispatch => ({
-  sendMessage: (message, profileId) => dispatch(sendMessage(message, profileId))
+    sendMessage: (message, profileId) => dispatch(sendMessage(message, profileId))
 });
 
 export default connect(
-  mapState,
-  mapDispatch
+    mapState,
+    mapDispatch
 )(RecruitmentProfileInfo);
