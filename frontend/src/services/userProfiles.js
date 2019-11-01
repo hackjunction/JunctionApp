aa2436069d963633b6163f1d3aae06b291ced041
petrus <holm.petrus@gmail.com>
2019-11-01 14:51:16 +0200

    Added full name for recruiter

--- frontend/src/services/userProfiles.js
+++ frontend/src/services/userProfiles.js
@@ -3,31 +3,31 @@ import _axios from 'services/axios';
 const UserProfilesService = {};
 
 function config(idToken) {
-    return {
-        headers: {
-            Authorization: `Bearer ${idToken}`
-        }
-    };
+  return {
+    headers: {
+      Authorization: `Bearer ${idToken}`
+    }
+  };
 }
 
 UserProfilesService.getPublicUserProfiles = userIds => {
-    return _axios.get('/user-profiles/public/', {
-        params: {
-            userIds
-        }
-    });
+  return _axios.get('/user-profiles/public/', {
+    params: {
+      userIds
+    }
+  });
 };
 
 UserProfilesService.getUserProfile = idToken => {
-    return _axios.get('/user-profiles/', config(idToken));
+  return _axios.get('/user-profiles/', config(idToken));
 };
 
 UserProfilesService.createUserProfile = (data, idToken) => {
-    return _axios.post('/user-profiles/', data, config(idToken));
+  return _axios.post('/user-profiles/', data, config(idToken));
 };
 
 UserProfilesService.updateUserProfile = (data, idToken) => {
-    return _axios.patch('/user-profiles/', data, config(idToken));
+  return _axios.patch('/user-profiles/', data, config(idToken));
 };
 
 // UserProfilesService.getUsersByEmail = (email, idToken) => {
@@ -38,20 +38,25 @@ UserProfilesService.updateUserProfile = (data, idToken) => {
 // };
 
 UserProfilesService.queryUsers = (idToken, terms) => {
-    return _axios.get(`/user-profiles/search/${terms}`, config(idToken));
+  return _axios.get(`/user-profiles/search/${terms}`, config(idToken));
 };
 
 UserProfilesService.getUserProfileRecruitment = (userId, idToken) => {
-    return _axios.get(`/recruitment/profile/${userId}`, config(idToken));
+  return _axios.get(`/recruitment/profile/${userId}`, config(idToken));
 };
 
 UserProfilesService.getRecruiters = idToken => {
-    return _axios.get('/user-profiles/recruiters', config(idToken));
+  return _axios.get('/user-profiles/recruiters', config(idToken));
 };
 
-UserProfilesService.updateRecruiter = (idToken, recruiterId, events, organisation) => {
-    const data = { recruiterId, events, organisation };
-    return _axios.patch('/user-profiles/recruiters', data, config(idToken));
+UserProfilesService.updateRecruiter = (
+  idToken,
+  recruiterId,
+  events,
+  organisation
+) => {
+  const data = { recruiterId, events, organisation };
+  return _axios.patch('/user-profiles/recruiters', data, config(idToken));
 };
 
 export default UserProfilesService;
