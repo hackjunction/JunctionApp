9e63e8eb20efe2039ad550036ea9678d108b0f5c
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-11-06 05:21:10 +0200

    Add sync-user-profiles, admin routes

---
+++ backend/common/middleware/admin.js
@@ -0,0 +1,13 @@
+const { NotFoundError } = require('../errors/errors');
+
+const AdminMiddleware = {
+    hasAdminToken: (req, res, next) => {
+        if (req.query.token === global.gConfig.ADMIN_TOKEN) {
+            next();
+        } else {
+            next(new NotFoundError());
+        }
+    }
+};
+
+module.exports = AdminMiddleware;
