864f4df6f3fc6d00adf0862a59ba330e69eda04c
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-11-11 18:24:53 +0200

    Fix date time input

--- frontend/src/components/inputs/DateTimeInput/index.js
+++ frontend/src/components/inputs/DateTimeInput/index.js
@@ -1,33 +1,39 @@
-import React, { useState, useCallback } from 'react';
-import moment from 'moment';
+import React, { useCallback } from 'react';
+import moment from 'moment-timezone';
 
 import { Grid } from '@material-ui/core';
 import DateInput from '../DateInput';
 import TimeInput from '../TimeInput';
 
 const DateTimeInput = ({ value, onChange, onBlur, timezone = 'UTC' }) => {
-    const momentValue = value && timezone ? moment(value).tz(timezone) : moment().startOf('day');
+    const momentValue =
+        value && timezone
+            ? moment(value).tz(timezone)
+            : moment()
+                  .tz(timezone)
+                  .startOf('day');
 
     const handleDateChange = useCallback(
         date => {
             if (!date) {
                 onChange(undefined);
             } else {
-                const newValue = moment(date);
-                newValue.year(newValue.year());
-                newValue.month(newValue.month());
-                newValue.date(newValue.date());
+                const newValue = momentValue.clone();
+                const dateAsMoment = moment(date);
+                newValue.year(dateAsMoment.year());
+                newValue.month(dateAsMoment.month());
+                newValue.date(dateAsMoment.date());
                 onChange(newValue.startOf('minute').format());
             }
         },
-        [onChange]
+        [onChange, momentValue]
     );
 
     const handleTimeChange = useCallback(
         ({ hours, minutes }) => {
             const newValue = momentValue.clone();
-            newValue.hours(hours);
-            newValue.minutes(40);
+            newValue.hour(hours);
+            newValue.minute(minutes);
             onChange(newValue.startOf('minute').format());
         },
         [momentValue, onChange]
