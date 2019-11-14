7da646b3ffe708c8bb6a865fefce90ea028d60bf
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-11-14 20:42:53 +0200

    Add placeholder page for reviewing

---
+++ frontend/src/pages/EventDashboard/EventDashboardReviewing/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Box } from '@material-ui/core';
+
+import PageHeader from 'components/generic/PageHeader';
+
+const EventDashboardReviewing = () => {
+    return (
+        <Box>
+            <PageHeader heading="Reviewing" />
+        </Box>
+    );
+};
+
+export default EventDashboardReviewing;
