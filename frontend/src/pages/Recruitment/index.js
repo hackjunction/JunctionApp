d4a9feeea309b14053fddb0bc8da74c080725d26
petrus <holm.petrus@gmail.com>
2019-11-16 15:00:47 +0200

    Only show participants of Main event 2019, remove event filter from frontend

--- frontend/src/pages/Recruitment/index.js
+++ frontend/src/pages/Recruitment/index.js
@@ -1,43 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import { connect } from 'react-redux';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from "react-redux";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import SearchPage from './Search';
-import AdminPage from './Admin';
-import DetailPage from './Detail';
-import PageWrapper from 'components/layouts/PageWrapper';
-import * as RecruitmentActions from 'redux/recruitment/actions';
-import CenteredContainer from 'components/generic/CenteredContainer';
-import BasicNavBar from 'components/navbars/BasicNavBar';
+import SearchPage from "./Search";
+import AdminPage from "./Admin";
+import DetailPage from "./Detail";
+import PageWrapper from "components/layouts/PageWrapper";
+import * as RecruitmentActions from "redux/recruitment/actions";
+import CenteredContainer from "components/generic/CenteredContainer";
+import BasicNavBar from "components/navbars/BasicNavBar";
 
-const RecruitmentPage = ({ location, match, updateEvents, updateActionHistory }) => {
-    useEffect(() => {
-        updateEvents();
-        updateActionHistory();
-    }, []);
+const RecruitmentPage = ({
+  location,
+  match,
+  updateEvents,
+  updateActionHistory
+}) => {
+  useEffect(() => {
+    updateEvents();
+    updateActionHistory();
+  }, []);
 
-    return (
-        <PageWrapper wrapContent={false} showErrorMessage>
-            <CenteredContainer>
-                <BasicNavBar text="Junction Recruitment Tool" />
-            </CenteredContainer>
-            <Route path="/recruitment" component={SearchPage} />
-            <Switch>
-                <Route exact path="/recruitment/admin" component={AdminPage} />
-                <Route path="/recruitment/:id" component={DetailPage} />
-                <Redirect to="/recruitment" />
-            </Switch>
-        </PageWrapper>
-    );
+  return (
+    <PageWrapper wrapContent={false} showErrorMessage>
+      <CenteredContainer>
+        <BasicNavBar text="Junction Recruitment Tool" />
+      </CenteredContainer>
+      <Route path="/recruitment" component={SearchPage} />
+      <Switch>
+        <Route exact path="/recruitment/admin" component={AdminPage} />
+        <Route path="/recruitment/:id" component={DetailPage} />
+        <Redirect to="/recruitment" />
+      </Switch>
+    </PageWrapper>
+  );
 };
 
 const mapDispatch = dispatch => ({
-    updateEvents: () => dispatch(RecruitmentActions.updateEvents()),
-    updateActionHistory: () => dispatch(RecruitmentActions.updateActionHistory())
+  updateEvents: () => dispatch(RecruitmentActions.updateEvents()),
+  updateActionHistory: () => dispatch(RecruitmentActions.updateActionHistory())
 });
 
-export default connect(
-    null,
-    mapDispatch
-)(RecruitmentPage);
+export default connect(null, mapDispatch)(RecruitmentPage);
