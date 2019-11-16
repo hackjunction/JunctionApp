03c549c4b05c7a6e735079716eedf3b5bf08c284
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-11-16 16:48:05 +0200

    Enable detail view in admin panel

---
+++ frontend/src/components/tables/ProjectsTable/index.js
@@ -0,0 +1,94 @@
+import React, { forwardRef, useCallback, useState } from 'react';
+
+import moment from 'moment-timezone';
+import MaterialTable from 'components/generic/MaterialTable';
+import ProjectDetail from 'components/projects/ProjectDetail';
+import { Paper, Dialog } from '@material-ui/core';
+import { EventHelpers } from '@hackjunction/shared';
+
+const ProjectsTable = ({ event, projects, loading }) => {
+    const [selected, setSelected] = useState();
+
+    const handleSelect = useCallback(project => {
+        setSelected(project);
+    }, []);
+
+    const handleClose = useCallback(() => {
+        setSelected();
+    }, []);
+
+    const isOngoingEvent = EventHelpers.isEventOngoing(event, moment);
+    return (
+        <React.Fragment>
+            <MaterialTable
+                title="Projects"
+                showCount
+                isLoading={loading}
+                data={projects}
+                onRowClick={(e, row) => handleSelect(row)}
+                // onSelectionChange={rows => setSelected(rows.map(r => r.user))}
+                // actions={
+                //     !minimal
+                //         ? [
+                //               {
+                //                   icon: forwardRef((props, ref) => <EmailIcon {...props} ref={ref} />),
+                //                   tooltip: 'Email selected',
+                //                   onClick: toggleBulkEmail
+                //               },
+                //               {
+                //                   icon: forwardRef((props, ref) => <EditIcon {...props} ref={ref} />),
+                //                   tooltip: 'Edit selected',
+                //                   onClick: toggleBulkEdit
+                //               }
+                //           ]
+                //         : []
+                // }
+                options={{
+                    exportButton: true,
+                    selection: false,
+                    showSelectAllCheckbox: false,
+                    pageSizeOptions: [5, 25, 50],
+                    debounceInterval: 500,
+                    search: true,
+                    paging: true
+                }}
+                localization={{
+                    toolbar: {
+                        searchPlaceholder: 'Search by name/email',
+                        nRowsSelected: '{0} selected'
+                    }
+                }}
+                components={{
+                    Container: forwardRef((props, ref) => <Paper {...props} ref={ref} />)
+                }}
+                columns={[
+                    {
+                        title: 'Name',
+                        field: 'name',
+                        searchable: true
+                    },
+                    {
+                        title: 'Location',
+                        field: 'location',
+                        searchable: true
+                    },
+                    {
+                        title: 'Punchline',
+                        field: 'punchline',
+                        searchable: true
+                    }
+                ]}
+            />
+            <Dialog transitionDuration={0} fullScreen open={Boolean(selected)} onClose={handleClose}>
+                <ProjectDetail
+                    project={selected}
+                    event={event}
+                    onBack={handleClose}
+                    showTableLocation={isOngoingEvent}
+                />
+            </Dialog>
+        </React.Fragment>
+    );
+};
+
+export default ProjectsTable;
--- frontend/src/pages/OrganiserDashboard/OrganiserEditEvent/Projects/Search/index.js
+++ frontend/src/pages/OrganiserDashboard/OrganiserEditEvent/Projects/Search/index.js
@@ -3,72 +3,13 @@ import { connect } from 'react-redux';
 import { Paper } from '@material-ui/core';
 
 import MaterialTable from 'components/generic/MaterialTable';
+import ProjectsTable from 'components/tables/ProjectsTable';
 
 import * as OrganiserSelectors from 'redux/organiser/selectors';
 import * as OrganiserActions from 'redux/organiser/actions';
 
 const SearchTab = ({ event, projects, projectsLoading }) => {
-    return (
-        <MaterialTable
-            title="Projects"
-            showCount
-            isLoading={projectsLoading}
-            data={projects}
-            // onRowClick={(e, row) => setEditing(row.user)}
-            // onSelectionChange={rows => setSelected(rows.map(r => r.user))}
-            // actions={
-            //     !minimal
-            //         ? [
-            //               {
-            //                   icon: forwardRef((props, ref) => <EmailIcon {...props} ref={ref} />),
-            //                   tooltip: 'Email selected',
-            //                   onClick: toggleBulkEmail
-            //               },
-            //               {
-            //                   icon: forwardRef((props, ref) => <EditIcon {...props} ref={ref} />),
-            //                   tooltip: 'Edit selected',
-            //                   onClick: toggleBulkEdit
-            //               }
-            //           ]
-            //         : []
-            // }
-            options={{
-                exportButton: true,
-                selection: false,
-                showSelectAllCheckbox: false,
-                pageSizeOptions: [5, 25, 50],
-                debounceInterval: 500,
-                search: true,
-                paging: true
-            }}
-            localization={{
-                toolbar: {
-                    searchPlaceholder: 'Search by name/email',
-                    nRowsSelected: '{0} selected'
-                }
-            }}
-            components={{
-                Container: forwardRef((props, ref) => <Paper {...props} ref={ref} />)
-            }}
-            columns={[
-                {
-                    title: 'Name',
-                    field: 'name',
-                    searchable: true
-                },
-                {
-                    title: 'Location',
-                    field: 'location',
-                    searchable: true
-                },
-                {
-                    title: 'Punchline',
-                    field: 'punchline',
-                    searchable: true
-                }
-            ]}
-        />
-    );
+    return <ProjectsTable projects={projects} event={event} loading={projectsLoading} />;
 };
 
 const mapState = state => ({
