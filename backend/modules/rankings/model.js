5b7f37b6b6c391844f62ba588d39d8c5cf41cab5
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-12-11 15:15:15 +0200

    Create rankings schema and controller

---
+++ backend/modules/rankings/model.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+/** Schema for storing event results */
+
+const RankingsSchema = new mongoose.Schema({
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+    },
+    tag: {
+        type: String,
+        enum: ['overall-winners', 'finalists']
+    },
+    track: {
+        type: String
+    },
+    challenge: {
+        type: String
+    },
+    rankings: [
+        {
+            project: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Project',
+                required: true
+            },
+            rank: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+/** Only allow a single rankings document per event/track combination */
+RankingsSchema.index(
+    {
+        event: 1,
+        track: 1
+    },
+    {
+        unique: true,
+        sparse: true
+    }
+);
+
+/** Only allow a single rankings document per event/challenge combination */
+RankingsSchema.index(
+    {
+        event: 1,
+        challenge: 1
+    },
+    {
+        unique: true,
+        sparse: true
+    }
+);
+
+/** Only allow a single rankings document per event/tag combination, e.g. overall winners */
+RankingsSchema.index(
+    {
+        event: 1,
+        tag: 1
+    },
+    {
+        unique: true,
+        sparse: true
+    }
+);
+
+const Rankings = mongoose.model('Rankings', RankingsSchema);
+
+module.exports = Rankings;
---
+++ shared/constants/overall-reviewing-methods.js
@@ -0,0 +1,16 @@
+const OverallReviewingMethods = {
+    finalsPublicVoting: {
+        id: 'finalsPublicVoting',
+        label: 'Finals, public voting'
+    },
+    finalsManualSelection: {
+        id: 'finalsManualSelection',
+        label: 'Finals, manual selection'
+    },
+    noOverallWinner: {
+        id: 'noOverallWinner',
+        label: 'No overall winner'
+    }
+};
+
+module.exports = OverallReviewingMethods;
