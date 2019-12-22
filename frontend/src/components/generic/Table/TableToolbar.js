bd9cb6c2a2611b3ff6a6825aeb666149df73cc82
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-12-22 21:09:33 +0200

    Replace classnames with clsx

--- frontend/src/components/generic/Table/TableToolbar.js
+++ frontend/src/components/generic/Table/TableToolbar.js
@@ -1,57 +1,79 @@
-import React from 'react';
+import React from 'react'
 
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import { Toolbar, Tooltip, Typography, Box, IconButton } from '@material-ui/core';
-
-import classNames from 'classnames';
+import { lighten, makeStyles } from '@material-ui/core/styles'
+import {
+    Toolbar,
+    Tooltip,
+    Typography,
+    Box,
+    IconButton,
+} from '@material-ui/core'
+import clsx from 'clsx'
 
 const useToolbarStyles = makeStyles(theme => ({
     root: {
         paddingLeft: theme.spacing(2),
         paddingRight: theme.spacing(1),
-        transition: 'all 0.2s ease'
+        transition: 'all 0.2s ease',
     },
     highlight: {
         color: theme.palette.primary.dark,
-        backgroundColor: lighten(theme.palette.primary.main, 0.85)
+        backgroundColor: lighten(theme.palette.primary.main, 0.85),
     },
     action: {
         transform: 'scale(0.5)',
         transition: 'all 0.2s ease',
         opacity: 0,
-        pointerEvents: 'none'
+        pointerEvents: 'none',
     },
     actionActive: {
         transform: 'scale(1)',
         opacity: 1,
-        pointerEvents: 'initial'
-    }
-}));
+        pointerEvents: 'initial',
+    },
+}))
 
 const TableToolbar = ({ title, selectedRows, actions = [] }) => {
-    const classes = useToolbarStyles();
-    const hasSelected = selectedRows.length > 0;
+    const classes = useToolbarStyles()
+    const hasSelected = selectedRows.length > 0
 
     return (
-        <Toolbar className={classNames(classes.root, { [classes.highlight]: hasSelected })}>
+        <Toolbar
+            className={clsx({
+                [classes.root]: true,
+                [classes.highlight]: hasSelected,
+            })}
+        >
             <Typography variant="button" color="inherit">
                 {hasSelected ? `${selectedRows.length} selected` : title}
             </Typography>
-            <Box m={1} flex="1" display="flex" flexDirection="row" justifyContent="flex-end">
+            <Box
+                m={1}
+                flex="1"
+                display="flex"
+                flexDirection="row"
+                justifyContent="flex-end"
+            >
                 {actions.map(action => (
                     <Tooltip
-                        className={classNames(classes.action, { [classes.actionActive]: hasSelected })}
+                        className={clsx({
+                            [classes.action]: true,
+                            [classes.actionActive]: hasSelected,
+                        })}
                         key={action.key}
                         title={action.label}
                     >
-                        <IconButton onClick={() => action.action(selectedRows)} aria-label={action.label}>
+                        <IconButton
+                            onClick={() => action.action(selectedRows)}
+                            aria-label={action.label}
+                        >
                             {action.icon}
                         </IconButton>
                     </Tooltip>
                 ))}
             </Box>
         </Toolbar>
-    );
-};
+    )
+}
 
-export default TableToolbar;
+export default TableToolbar
