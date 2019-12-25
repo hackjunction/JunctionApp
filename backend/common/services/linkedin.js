1543fb76122df583de83ff56cba15ae5e7f05215
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-12-25 15:49:35 +0200

    Update logger configurations

--- backend/common/services/linkedin.js
+++ backend/common/services/linkedin.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
+const axios = require('axios')
 
-const CLIENT_ID = '77gyr30dlraeob';
-const CLIENT_SECRET = 'jvMviXjTgjluzXN8';
+const CLIENT_ID = '77gyr30dlraeob'
+const CLIENT_SECRET = 'jvMviXjTgjluzXN8'
 
 const LinkedInService = {
     getAccessToken: () => {
@@ -12,17 +12,17 @@ const LinkedInService = {
                 {
                     headers: {
                         Host: 'www.linkedin.com',
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
                 }
             )
             .then(res => {
-                console.log('getAccessToken', res);
+                console.log('getAccessToken', res)
             })
             .catch(err => {
-                console.log('getAccessToken err', err);
-            });
-    }
-};
+                console.log('getAccessToken err', err)
+            })
+    },
+}
 
-module.exports = LinkedInService;
+module.exports = LinkedInService
