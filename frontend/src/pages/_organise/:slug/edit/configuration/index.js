b40382713a4706d9da5da338cec02bc82839a223
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-01-08 12:59:11 +0200

    Hide some config fields dynamically

--- frontend/src/pages/_organise/:slug/edit/configuration/index.js
+++ frontend/src/pages/_organise/:slug/edit/configuration/index.js
@@ -273,27 +273,30 @@ export default () => {
             <Grid item xs={12}>
                 <Field
                     name="overallReviewMethod"
-                    render={({ field, form }) => (
-                        <FormControl
-                            label="Overall winner method"
-                            hint="This event is using tracks - which method should be used to determine the overall winner?"
-                            error={form.errors[field.name]}
-                            touched={form.touched[field.name]}
-                        >
-                            <Select
-                                value={field.value}
-                                onChange={value =>
-                                    form.setFieldValue(field.name, value)
-                                }
-                                options={Object.keys(
-                                    OverallReviewingMethods
-                                ).map(key => ({
-                                    label: OverallReviewingMethods[key].label,
-                                    value: key,
-                                }))}
-                            />
-                        </FormControl>
-                    )}
+                    render={({ field, form }) =>
+                        form.values.tracksEnabled && (
+                            <FormControl
+                                label="Overall winner method"
+                                hint="This event is using tracks - which method should be used to determine the overall winner?"
+                                error={form.errors[field.name]}
+                                touched={form.touched[field.name]}
+                            >
+                                <Select
+                                    value={field.value}
+                                    onChange={value =>
+                                        form.setFieldValue(field.name, value)
+                                    }
+                                    options={Object.keys(
+                                        OverallReviewingMethods
+                                    ).map(key => ({
+                                        label:
+                                            OverallReviewingMethods[key].label,
+                                        value: key,
+                                    }))}
+                                />
+                            </FormControl>
+                        )
+                    }
                 ></Field>
             </Grid>
             <Grid item xs={12}>
