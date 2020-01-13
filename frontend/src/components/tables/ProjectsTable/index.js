15f0c90bb43bd59df225ad0fb3a2c14c45f0242d
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-01-13 16:29:47 +0200

    Move ProjectsTable under components/tables

--- frontend/src/components/tables/ProjectsTable/index.js
+++ frontend/src/components/tables/ProjectsTable/index.js
@@ -1,101 +1,37 @@
-import React, { forwardRef, useCallback, useState } from 'react'
+import React, { useMemo } from 'react'
 
-import moment from 'moment-timezone'
-import MaterialTable from 'components/generic/MaterialTable'
-import ProjectDetail from 'components/projects/ProjectDetail'
-import { Paper, Dialog } from '@material-ui/core'
-import { EventHelpers } from '@hackjunction/shared'
+import { Table, Filters, Sorters } from 'components/generic/_Table'
 
-const ProjectsTable = ({ event, projects, loading }) => {
-    const [selected, setSelected] = useState()
-
-    const handleSelect = useCallback(project => {
-        setSelected(project)
+const ProjectsTable = ({ projects }) => {
+    const columns = useMemo(() => {
+        return [
+            {
+                Header: '#',
+                accessor: (row, index) => {
+                    return index + 1
+                },
+                id: 'index',
+                sortType: Sorters.Numeric,
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+                ...Filters.ContainsSearch,
+            },
+            {
+                Header: 'Punchline',
+                accessor: 'punchline',
+                ...Filters.ContainsSearch,
+            },
+            {
+                Header: 'Location',
+                accessor: 'location',
+                ...Filters.ContainsSearch,
+            },
+        ]
     }, [])
 
-    const handleClose = useCallback(() => {
-        setSelected()
-    }, [])
-
-    const isOngoingEvent = EventHelpers.isEventOngoing(event, moment)
-    return (
-        <React.Fragment>
-            <MaterialTable
-                title="Projects"
-                showCount
-                isLoading={loading}
-                data={projects}
-                onRowClick={(e, row) => handleSelect(row)}
-                // onSelectionChange={rows => setSelected(rows.map(r => r.user))}
-                // actions={
-                //     !minimal
-                //         ? [
-                //               {
-                //                   icon: forwardRef((props, ref) => <EmailIcon {...props} ref={ref} />),
-                //                   tooltip: 'Email selected',
-                //                   onClick: toggleBulkEmail
-                //               },
-                //               {
-                //                   icon: forwardRef((props, ref) => <EditIcon {...props} ref={ref} />),
-                //                   tooltip: 'Edit selected',
-                //                   onClick: toggleBulkEdit
-                //               }
-                //           ]
-                //         : []
-                // }
-                options={{
-                    exportButton: true,
-                    selection: false,
-                    showSelectAllCheckbox: false,
-                    pageSizeOptions: [5, 10, 25, 50],
-                    debounceInterval: 500,
-                    search: true,
-                    paging: true,
-                    pageSize: 10,
-                }}
-                localization={{
-                    toolbar: {
-                        searchPlaceholder: 'Search projects',
-                        nRowsSelected: '{0} selected',
-                    },
-                }}
-                components={{
-                    Container: forwardRef((props, ref) => (
-                        <Paper {...props} ref={ref} />
-                    )),
-                }}
-                columns={[
-                    {
-                        title: 'Name',
-                        field: 'name',
-                        searchable: true,
-                    },
-                    {
-                        title: 'Punchline',
-                        field: 'punchline',
-                        searchable: true,
-                    },
-                    {
-                        title: 'Location',
-                        field: 'location',
-                    },
-                ]}
-            />
-            <Dialog
-                transitionDuration={0}
-                fullScreen
-                open={Boolean(selected)}
-                onClose={handleClose}
-            >
-                <ProjectDetail
-                    project={selected}
-                    event={event}
-                    onBack={handleClose}
-                    showTableLocation={isOngoingEvent}
-                />
-            </Dialog>
-        </React.Fragment>
-    )
+    return <Table data={projects} columns={columns} />
 }
 
 export default ProjectsTable
--- frontend/src/pages/_organise/:slug/projects/default/index.js
+++ frontend/src/pages/_organise/:slug/projects/default/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import * as OrganiserSelectors from 'redux/organiser/selectors'
-import ProjectsTable from '../ProjectsTable'
+import ProjectsTable from 'components/tables/ProjectsTable'
 
 export default () => {
     const projects = useSelector(OrganiserSelectors.projects)
