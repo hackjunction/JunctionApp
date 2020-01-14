f0cc4c1edae0c1376be291ee902e11d0ce43a754
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-01-14 02:47:06 +0200

    Autolint user-profiles

--- backend/modules/user-profile/helpers.js
+++ backend/modules/user-profile/helpers.js
@@ -1,24 +1,24 @@
-const { RegistrationFields } = require('@hackjunction/shared');
-const yup = require('yup');
+const { RegistrationFields } = require('@hackjunction/shared')
+const yup = require('yup')
 
 const UserProfileHelpers = {
     validate: data => {
-        const validations = {};
+        const validations = {}
         Object.keys(data).forEach(field => {
-            const fieldConfig = RegistrationFields.getField(field);
+            const fieldConfig = RegistrationFields.getField(field)
             if (fieldConfig) {
-                validations[field] = fieldConfig.validationSchema(false);
+                validations[field] = fieldConfig.validationSchema(false)
             }
-        });
+        })
 
-        validations['avatar'] = yup
+        validations.avatar = yup
             .string()
             .url()
-            .nullable();
+            .nullable()
 
-        const schema = yup.object().shape(validations);
-        return schema.validate(data, { stripUnknown: true });
-    }
-};
+        const schema = yup.object().shape(validations)
+        return schema.validate(data, { stripUnknown: true })
+    },
+}
 
-module.exports = UserProfileHelpers;
+module.exports = UserProfileHelpers
