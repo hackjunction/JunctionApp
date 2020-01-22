7da421cffd4618015f67001cb9fd668b7319480d
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-01-22 20:14:00 +0200

    Set up barebones graphql stuff for Events

--- backend/modules/event/graphql-controller.js
+++ backend/modules/event/graphql-controller.js
@@ -1,9 +1,57 @@
+const { Auth } = require('@hackjunction/shared')
 const Event = require('./model')
+const PermissionUtils = require('../../utils/permissions')
 
-const controller = {}
+class EventController {
+    constructor(requestingUser, overrideChecks) {
+        this.requestingUser = requestingUser
+        this.overrideChecks = overrideChecks
+        this.isAdmin =
+            overrideChecks ||
+            PermissionUtils.userHasPermission(
+                requestingUser,
+                Auth.Permissions.MANAGE_EVENT
+            )
+    }
 
-controller.getById = eventId => {
-    return Event.findById(eventId)
+    getById(eventId) {
+        return this._clean(Event.findById(eventId))
+    }
+
+    async _clean(promise) {
+        const result = await promise
+        if (Array.isArray(result)) {
+            const results = result.map(item => {
+                return this._cleanOne(item)
+            })
+            return results.filter(item => item !== null)
+        }
+        return this._cleanOne(result)
+    }
+
+    _cleanOne(event) {
+        if (!event) return null
+
+        /** If it's a public event, anyone can see it */
+        if (event.published) {
+            // TODO: Maybe strip some fields from the response if necessary?
+            return event
+        }
+
+        /** If the user is an organiser or admin, they can see it */
+        const { isAdmin } = this
+        const isOwner =
+            this.requestingUser && this.requestingUser.sub === event.owner
+        const isOrganiser =
+            this.requestingUser &&
+            event.organisers.indexOf(this.requestingUser.sub) !== -1
+        if (isAdmin || isOwner || isOrganiser) {
+            return event
+        }
+
+        /** Otherwise return null for now */
+        return null
+    }
 }
 
-module.exports = controller
+module.exports = EventController
