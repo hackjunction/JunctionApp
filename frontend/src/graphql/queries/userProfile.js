d1aaf7f75abdc67ed37930f4e0850c4eb53f4507
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-01-24 18:24:03 +0200

    Change UserMenu to use myProfile query

--- frontend/src/graphql/queries/userProfile.js
+++ frontend/src/graphql/queries/userProfile.js
@@ -1,20 +1,47 @@
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
 
-export const GET_USER_AVATAR = gql`
+const Fragments = {
+    ProfilePreview: gql`
+        fragment ProfilePreview on UserProfile {
+            avatar
+            firstName
+            lastName
+            email
+        }
+    `,
+}
+
+export const GET_PROFILE_PREVIEW = gql`
     query UserProfile($userId: ID!) {
         userProfileById(userId: $userId) {
-            avatar
+            ...ProfilePreview
         }
     }
+    ${Fragments.ProfilePreview}
 `
 
 export const useUserAvatar = userId => {
-    const { data, loading, error } = useQuery(GET_USER_AVATAR, {
+    const { data, loading, error } = useQuery(GET_PROFILE_PREVIEW, {
         variables: {
             userId,
         },
     })
 
     return [data?.userProfileById?.avatar, loading, error]
+}
+
+export const GET_MY_PROFILE_PREVIEW = gql`
+    query UserProfile {
+        myProfile {
+            ...ProfilePreview
+        }
+    }
+    ${Fragments.ProfilePreview}
+`
+
+export const useMyProfilePreview = () => {
+    const { data, loading, error } = useQuery(GET_MY_PROFILE_PREVIEW)
+
+    return [data?.myProfile, loading, error]
 }
