5538d2448a376e0157daee5ee4840840d9601dd8
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-02-06 23:35:43 +0200

    cleaning up

--- backend/index.js
+++ backend/index.js
@@ -57,25 +57,26 @@ app.use(errors())
 app.use(require('./common/errors/errorHandler'))
 
 /* Database connection */
-const database = require('./misc/db')
+require('./misc/db').connect()
 
-database.connect()
-// /* Migrations to run before exposing the server */
 const migrations = require('./migrations/index')
 
+/** A clone of the npm library throng, with a minor edit. See the file for details. */
 const throng = require('./misc/throng')
 
 throng({
     workers: process.env.WEB_CONCURRENCY || 1,
     grace: 1000,
     lifetime: Infinity,
+    /** Start the master process. The server will start the slave processes
+     *  (and thus begin accepting requests) only once this function has resolved...
+     */
     master: async () => {
         logger.info(`Master ${process.pid} started`)
+        // ...so this is a good place to run e.g. database migrations
         await migrations.run()
-        /** Run cron jobs here for now, migrate to cron-cluster later */
-        // cron.utils.startAll();
     },
-    /** Start the slave processes (1-n) */
+    /** Start the slave processes (1-n), which listen for incoming requests */
     start: () => {
         const PORT = process.env.PORT || 2222
 
@@ -85,8 +86,9 @@ throng({
             )
         })
     },
-    /** This is run only if the master function errors out. Workers are automatically
-     * revived on failure.
+    /** This is run only if the master function errors out, which means the
+     *  server could not start properly. Workers are automatically revived on failure, if e.g.
+     *  the app crashes or runs out of memory while processing a request.
      */
     onError: err => {
         logger.error({
