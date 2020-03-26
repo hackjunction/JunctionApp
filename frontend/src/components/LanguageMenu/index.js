25ddd636188f6de7284ec0ead0a846c84ff1afec
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-03-26 14:02:45 +0200

    add few more translation example
    (cherry picked from commit 852a8ef4c757ff5b75db7209965dd9bf9156cdda)

--- frontend/src/components/LanguageMenu/index.js
+++ frontend/src/components/LanguageMenu/index.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import { useTranslation } from 'react-i18next'
-import Icon from '@material-ui/core/Icon'
 
 export default () => {
     const { t, i18n } = useTranslation()
@@ -11,7 +10,6 @@ export default () => {
     const handleChange = event => {
         i18n.changeLanguage(event.target.value)
     }
-    console.log('langes ares', i18n.languages)
     return (
         <Select
             labelId="demo-simple-select-label"
