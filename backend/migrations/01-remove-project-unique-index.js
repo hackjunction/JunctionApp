0feae6dae13d8fbd0dc6fafc1f2302881821b840
Gergely Bihary <gergely.bihary@prezi.com>
2020-03-29 18:38:12 +0200

    Update migration

--- backend/migrations/01-remove-project-unique-index.js
+++ backend/migrations/01-remove-project-unique-index.js
@@ -1,39 +1,3 @@
-/** Change the way registration questions are stored in the Event model.
- *  Previously events had a rather strange UserDetailsConfig object for defining
- *  which questions to ask in the registration form. This script makes the following
- *  change to the event model:
- *
- *  Before:
- *  {
- *    userDetailsConfig: {
- *      phoneNumber: {
- *          enabled: true,
- *          required: true,
- *          editable: true
- *      },
- *      countryOfResidence: {
- *          enabled: true,
- *          required: false,
- *          editable: true
- *      },
- *      ...(all other fields here)
- *    }
- *  }
- *
- *  After:
- *  {
- *    registrationConfig: {
- *      optionalFields: ['countryOfResidence'],
- *      requiredFields: ['phoneNumber'],
- *    }
- *  }
- *
- *  In addition, the "editable" field which was used just for disallowing
- *  removing firstName, lastName, email from the registration questions, is
- *  replaced with just hardcoding this condition in code rather than in the
- *  event model.
- */
-
 const mongoose = require('mongoose')
 const Promise = require('bluebird')
 const logger = require('../misc/logger')
@@ -53,7 +17,7 @@ module.exports = {
                 .collection.dropIndex(indexToDelete.name)
             logger.info(`Unique index dropped`)
         } else {
-            logger.info(`-> No indexes to deletee`)
+            logger.info(`-> No indexes to delete`)
         }
         return Promise.resolve()
     },
