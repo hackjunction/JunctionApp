935c953cc8cb1f1ac5a5424fe4726ad636d13456
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-04-09 15:57:52 +0300

    Very hardcoded but working prototype

--- frontend/src/pages/_hackerpack/index.js
+++ frontend/src/pages/_hackerpack/index.js
@@ -4,18 +4,24 @@ import { Box, Typography, Divider } from '@material-ui/core'
 import CompanySection from './CompanySection'
 
 import PageHeader from 'components/generic/PageHeader'
+import Footer from 'components/layouts/Footer'
 import PageWrapper from 'components/layouts/PageWrapper'
-
+import GlobalNavBar from 'components/navbars/GlobalNavBar'
+import CenteredContainer from 'components/generic/CenteredContainer'
 import Partners from 'constants/hackerpack-partners.js'
 
 export default () => {
     return (
-        <React.Fragment>
-            <PageHeader
-                heading="Hackerpack"
-                subheading="We want you to be able to fully focus on making your hackathon project as cool as possible! These software provided by our partners will help you unleash your creativity and maximize your learning during our events."
-            />
-            <PageWrapper loading={false}>
+        <PageWrapper
+            loading={false}
+            header={() => <GlobalNavBar />}
+            footer={() => <Footer />}
+        >
+            <CenteredContainer>
+                <PageHeader
+                    heading="Hackerpack"
+                    subheading="We want you to be able to fully focus on making your hackathon project as cool as possible! These software provided by our partners will help you unleash your creativity and maximize your learning during our events."
+                />
                 <Divider variant="middle" />
                 {Partners.map(company => (
                     <React.Fragment>
@@ -37,7 +43,9 @@ export default () => {
                         suggestion.
                     </Typography>
                 </Box>
-            </PageWrapper>
-        </React.Fragment>
+            </CenteredContainer>
+        </PageWrapper>
+
+        // </React.Fragment>
     )
 }
