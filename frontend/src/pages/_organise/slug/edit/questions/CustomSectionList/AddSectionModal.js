d7bfae11af5bc87ef9fd5ed30e13771b5e5fb872
1UPNuke <65999898+1UPNuke@users.noreply.github.com>
2020-08-11 10:33:37 +0300

    Revert "Update AddSectionModal.js"
    This reverts commit 2db9f4462069f0e717274b97d59d0f539b48de84.

--- frontend/src/pages/_organise/slug/edit/questions/CustomSectionList/AddSectionModal.js
+++ frontend/src/pages/_organise/slug/edit/questions/CustomSectionList/AddSectionModal.js
@@ -55,7 +55,7 @@ export default ({
             return 'Please give your section a machine name'
         }
         
-        if (!(/^[a-z-]*$/).test(data.name)) {
+        if ((/^[a-z-]*$/).test(data.name)) {
             return 'Machine name can only contain lowercase characters and dashes (-)'
         }
 
