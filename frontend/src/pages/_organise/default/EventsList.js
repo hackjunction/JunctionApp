9ac63f92e695e96bb8707d4ea22686c82053cc9b
Jussi Impiö <jussi.impio@gmail.com>
2021-05-26 15:04:13 +0300

    Use excisting components for event search

--- frontend/src/pages/_organise/default/EventsList.js
+++ frontend/src/pages/_organise/default/EventsList.js
@@ -9,6 +9,9 @@ import { useTranslation } from 'react-i18next'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
+import SearchIcon from '@material-ui/icons/Search'
+import TextInput from '../../../components/inputs/TextInput'
+
 /*
 textShadow:
     '-1px -1px 0 green, 1px -1px 0 green, -1px 1px 0 green, 1px 1px 0 green',
@@ -48,9 +51,9 @@ export default ({ events = [] }) => {
     const [searchTerm, setSearchTerm] = React.useState('')
     const [searchResults, setSearchResults] = useState(events)
 
-    const handleChange = event => {
-        setSearchTerm(event.target.value)
-    }
+    // const handleChange = event => {
+    //     setSearchTerm(event.target.value)
+    // }
 
     useEffect(() => {
         const results = events.filter(
@@ -69,12 +72,24 @@ export default ({ events = [] }) => {
             <Typography variant="h6" gutterBottom>
                 {t('Your_events_')}
             </Typography>
-            <input
-                type="text"
-                placeholder="search"
-                value={searchTerm}
-                onChange={handleChange}
-            />
+            <Grid
+                container
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
+                spacing={1}
+            >
+                <Grid item>
+                    <SearchIcon />
+                </Grid>
+                <Grid item xs>
+                    <TextInput
+                        value={searchTerm}
+                        onChange={setSearchTerm}
+                        placeholder={t('Search_')}
+                    />
+                </Grid>
+            </Grid>
             <Grid container spacing={3}>
                 {searchResults.map(event => (
                     <Grid item xs={12} md={6} lg={4}>
