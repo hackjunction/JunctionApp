a6c02704acc56142e88603e847c3f578dfe8a962
Defelot <defelot@gmail.com>
2022-05-21 12:21:04 +0300

    render timeline on dashboard (#4)
    Co-authored-by: Leevi Rönkkö <leevi.ronkko@aalto.fi>

--- frontend/src/pages/_events/slug/context.js
+++ frontend/src/pages/_events/slug/context.js
@@ -53,6 +53,12 @@ const eventQuery = gql`
             frontPagePriority
             approved
             eventTerms
+            eventTimeline {
+                items {
+                    title
+                    startTime
+                }
+            }
             theme {
                 headerBackgroundColor
                 headerTextColor
