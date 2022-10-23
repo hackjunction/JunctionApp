faef7567e9e85fbe3ca1e89e674d546e98c39e7f
eficnbo <einomie@gmail.com>
2022-10-23 16:02:50 +0300

    href instead of hostname

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -30,7 +30,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 export default idToken => {
     const wsLink = new GraphQLWsLink(
         createClient({
-            url: 'ws://' +window.location.hostname+ ':2222/graphql',
+            url: 'ws://' +window.location.href+ ':2222/graphql',
             connectionParams: { authToken: idToken },
         }),
     )
