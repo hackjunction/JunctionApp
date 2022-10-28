f1431351f472322f44384db6dd4e1d22e1208652
eino <eino.miettinen@hackjunction.com>
2022-10-28 10:09:00 +0300

    stashed timeline

--- frontend/src/pages/_dashboard/slug/default/index.js
+++ frontend/src/pages/_dashboard/slug/default/index.js
@@ -80,7 +80,6 @@ export default ({ alerts }) => {
                         padding: '2em',
                     }}
                 >
-                    <TimeLineBlock />
                     <AlertBlock alerts={alerts} />
                 </div>
                 <EventOverBlock />
