798d2e36f74dc578d8607f9e2af5412116ba309e
Eficnbo <einomie@gmail.com>
2022-11-04 01:23:02 +0200

    migrationt

--- backend/migrations/08-create-registration-objects-in-profiles.js
+++ backend/migrations/08-create-registration-objects-in-profiles.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 const Promise = require('bluebird')
 
 const UserProfileControlller = require('../modules/user-profile/controller')
-
+// event":ObjectId("61012f8b31dc320049369a96"
 module.exports = {
     index: 8,
     name: '08-create-registration-objects-in-profiles',
     description: 'Sync registrationa and profile data',
     run: async () => {
-        const cursor = mongoose.model('Registration').find({}).cursor()
+        const cursor = mongoose
+            .model('Registration')
+            .find({ event: '62cd62fcfb0cc900455212fb' })
+            .cursor()
+
         await cursor.eachAsync(async function (registration) {
             UserProfileControlller.syncRegistration(registration)
         })
