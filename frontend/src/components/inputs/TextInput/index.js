5031997d217012f16bdf241f49720b4caae97379
David Amebley <davameb@gmail.com>
2023-07-15 17:41:17 +0300

    Update TextInput component to accept a name prop

--- frontend/src/components/inputs/TextInput/index.js
+++ frontend/src/components/inputs/TextInput/index.js
@@ -6,8 +6,9 @@ const TextInput = React.memo(
     ({
         disabled,
         label,
+        name,
         onBlur,
-        onChange = () => {},
+        onChange = () => { },
         placeholder = '',
         required = false,
         value = '',
@@ -27,6 +28,7 @@ const TextInput = React.memo(
                 disabled={disabled}
                 fullWidth
                 label={label}
+                name={name}
                 onBlur={onBlur}
                 onChange={handleChange}
                 placeholder={placeholder}
