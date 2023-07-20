ccf7d05eaba7e3920f3ddda118f4caaaf9f91fa6
Javier <javidx@gmail.com>
2023-07-20 21:14:34 +0300

    Removed commented code from Filter component

--- frontend/src/components/Team/Filter/index.js
+++ frontend/src/components/Team/Filter/index.js
@@ -20,32 +20,7 @@ export default ({ filterArray = [], noFilterOption = 'All' }) => {
                 options={filterArray}
                 onChange={handleChange}
             />
-            {/* <Select value={challengeFilter} onChange={handleChallengeChange}>
-            {challenges.map((challenge, index) => (
-              <option key={index} value={challenge.name}>
-                {challenge.name}
-              </option>
-            ))}
-      </Select> */}
             <Button onClick={resetFilters}>Reset filter</Button>
         </div>
     )
 }
-
-// function CategorySelector() {
-//   const dispatch = useDispatch();
-//   const { categories, categoryFilter } = useSelector((store) => store.categories);
-
-//   const handleSelectChange = (event) => {
-//     dispatch(applyFilter(event.target.value));
-//   };
-//   return (
-//     <select className="px-2 pb-1 pt-2 text-sky-900 bg-white" value={categoryFilter} onChange={handleSelectChange}>
-//       {categories.map((category) => (
-//         <option key={category} value={category}>
-//           {category}
-//         </option>
-//       ))}
-//     </select>
-//   );
-// }
