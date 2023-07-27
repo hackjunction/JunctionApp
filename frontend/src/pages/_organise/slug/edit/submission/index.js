7a53328062d133ce4dcf76246aae26deefc687b7
Javier <javidx@gmail.com>
2023-07-27 18:06:01 +0200

    Created temporal component using the customQuestion modal as starting point to explore options to create custom form fields

--- frontend/src/pages/_organise/slug/edit/submission/index.js
+++ frontend/src/pages/_organise/slug/edit/submission/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 import Section from './section'
+import { FastField } from 'formik'
+import FormControl from 'components/inputs/FormControl'
+import CustomSectionList from '../questions/CustomSectionList'
+import CustomSectionListItem from '../questions/CustomSectionList/CustomSectionListItem'
+import TempFieldBuilder from './components/TempFieldBuilder'
 // import { Formik } from 'formik'
 
 export default () => {
@@ -30,6 +35,64 @@ export default () => {
                     />
                 </div>
             ))}
+            {/* <FastField
+                name="submissionFormFields"
+                render={({ field, form }) => (
+                    <FormControl
+                        label="Custom questions"
+                        hint="Add custom registration questions"
+                    >
+                        <CustomSectionList
+                            sections={field.value}
+                            onChange={value =>
+                                form.setFieldValue(field.name, value)
+                            }
+                        />
+                    </FormControl>
+                )}
+            /> */}
+            {/* <CustomSectionListItem
+                key={'123'}
+                section={[
+                    {
+                        questions: ['test', 'there'],
+                        label: 'label test',
+                        description: 'description test',
+                        conditional: false,
+                    },
+                ]}
+                onChange={() => {}}
+                onRemove={() => {}}
+                onMoveUp={() => {}}
+                onMoveDown={() => {}}
+                onEdit={() => {}}
+                isFirst={true}
+                isLast={true}
+                // onChange={section => handleChange(section, index)}
+                // onRemove={() => handleRemove(section, index)}
+                // onMoveUp={() => handleMoveUp(section, index)}
+                // onMoveDown={() => handleMoveDown(section, index)}
+                // onEdit={() => setEditing(section)}
+                // isFirst={index === 0}
+                // isLast={index === sections.length - 1}
+            /> */}
+            <FastField name="submissionFormFields">
+                {({ field, form }) => <TempFieldBuilder />}
+            </FastField>
+            {/* render={({ field, form }) => (
+                        <FormControl
+                            label="Custom questions"
+                            hint="Add custom registration questions"
+                        >
+                            <CustomSectionList
+                                sections={field.value}
+                                onChange={value =>
+                                    form.setFieldValue(field.name, value)
+                                }
+                            />
+                        </FormControl>
+                    )}
+                /> */}
             <button
                 className="tw-mt-4 tw-bg-blue-500 tw-text-white tw-rounded-lg tw-px-4 tw-py-2 tw-cursor-pointer tw-shadow-md hover:tw-bg-blue-600"
                 onClick={handleAddSection}
