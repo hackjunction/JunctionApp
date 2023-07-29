7df3794d3b0977aadf076e51384563414836e389
samurotko <68745520+samurotko@users.noreply.github.com>
2023-07-29 13:55:18 +0300

    Update 17-add-specific-hackerpacks-to-event.js

--- backend/migrations/17-add-specific-hackerpacks-to-event.js
+++ backend/migrations/17-add-specific-hackerpacks-to-event.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Promise = require('bluebird')
 
 module.exports = {
-    index: 16,
-    name: '16-add-specific-hackerpacks-to-event',
+    index: 17,
+    name: '17-add-specific-hackerpacks-to-event',
     description: 'Add specifc hackerpacks to event',
     run: async () => {
         const res = await mongoose.model('Event').updateMany(
