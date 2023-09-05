d0b0765ba63fca1ab0f3ba6029cb73d93041915d
Javier <javidx@gmail.com>
2023-09-05 21:31:59 +0300

    Updated candidate schema to not require data fiels which could hold stale data

--- shared/schemas/Candidate.js
+++ shared/schemas/Candidate.js
@@ -4,22 +4,18 @@ const TeamRole = require('./TeamRole')
 const mongooseSchema = new mongoose.Schema({
     userId: {
         type: String,
-        required: true,
     },
     firstName: {
         type: String,
-        required: true,
     },
     lastName: {
         type: String,
-        required: true,
     },
     headline: {
         type: String,
     },
     avatar: {
         type: String,
-        required: false,
     },
     roles: {
         type: [TeamRole.mongoose],
