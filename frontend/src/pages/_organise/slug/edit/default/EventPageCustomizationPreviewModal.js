2439bb3f4bbef6a341349ea8ab0fbdc3254b5471
David Amebley <davameb@gmail.com>
2024-02-24 01:59:17 +0200

    Add initial code for the structure of the EventPageCustomizationPreviewModal component to show preview of customized event page

---
+++ frontend/src/pages/_organise/slug/edit/default/EventPageCustomizationPreviewModal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Dialog, DialogTitle, DialogContent, Button } from '@material-ui/core'
+
+export default function PreviewModal({ open, onClose, eventSlug }) {
+    return (
+        <Dialog
+            fullWidth
+            maxWidth="lg"
+            open={open}
+            onClose={onClose}
+            aria-labelledby="preview-modal-title"
+        >
+            <DialogTitle id="preview-modal-title">Event Preview</DialogTitle>
+            <DialogContent>
+                {/* Embedding the event page preview here. We could use an iframe or any other component that renders the preview */}
+                <iframe
+                    // src={`/events/${eventSlug}`}
+                    style={{ width: '100%', height: '500px', border: 'none' }}
+                    title="Event Preview"
+                />
+            </DialogContent>
+            <Button onClick={onClose} color="primary" variant="contained">
+                Close
+            </Button>
+        </Dialog>
+    )
+}
