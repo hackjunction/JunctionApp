cc61940cbbff946d966cc73a12448c78dce60835
wickathou <javier@enddesign.co>
2024-05-06 15:27:32 +0300

    Removed sslRedirect for Heroku Disabled RedisPubSub and Redis from alerts graphql config Disabled Redis from graphql config for message Modified addGavelLoginToRegistrations to perform only one iterations on gavelData, before it was done twice Modified teams page tabs label copy

--- backend/modules/alert/graphql.js
+++ backend/modules/alert/graphql.js
@@ -1,5 +1,5 @@
 const { withFilter } = require('graphql-subscriptions')
-const { RedisPubSub } = require('graphql-redis-subscriptions')
+// const { RedisPubSub } = require('graphql-redis-subscriptions')
 const {
     GraphQLString,
     GraphQLObjectType,
@@ -11,16 +11,12 @@ const {
 const { GraphQLDate } = require('graphql-iso-date')
 const RegistrationController = require('../registration/controller')
 const Event = require('../event/model')
-const Redis = require('ioredis')
-//const client = new Redis("rediss://default:JSVhlHFiXTnFo1Z1IVok05TOQqccA2qB@redis-11912.c226.eu-west-1-3.ec2.cloud.redislabs.com:11912");
-//console.log(client,"##")
+// const Redis = require('ioredis')
 
-
-const pubsub = new RedisPubSub({
-    publisher: new Redis(process.env.REDISCLOUD_URL),
-    subscriber: new Redis(process.env.REDISCLOUD_URL)
-})
-
+// const pubsub = new RedisPubSub({
+//     publisher: new Redis(process.env.REDISCLOUD_URL),
+//     subscriber: new Redis(process.env.REDISCLOUD_URL),
+// })
 
 const AlertInput = new GraphQLInputObjectType({
     name: 'AlertInput',
@@ -120,13 +116,13 @@ const Resolvers = {
                 throw new Error('You are not an organiser of this event')
             }
 
-            pubsub.publish('ALERT_SENT', {
-                newAlert: {
-                    ...args.alert,
-                    sentAt: new Date(),
-                    sender: userId,
-                },
-            })
+            // pubsub.publish('ALERT_SENT', {
+            //     newAlert: {
+            //         ...args.alert,
+            //         sentAt: new Date(),
+            //         sender: userId,
+            //     },
+            // })
 
             return context.controller('Alert').send(args.alert, userId)
         },
@@ -135,9 +131,9 @@ const Resolvers = {
         newAlert: {
             subscribe: withFilter(
                 () => {
-                    return pubsub.asyncIterator('ALERT_SENT')
+                    // return pubsub.asyncIterator('ALERT_SENT')
                 },
-                async ({newAlert}, { eventId, slug }, { user }) => {
+                async ({ newAlert }, { eventId, slug }, { user }) => {
                     // Check authentication from context
                     const userId = user ? user.sub : null
                     if (!userId) {
