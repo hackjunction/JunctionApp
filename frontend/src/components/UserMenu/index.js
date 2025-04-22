130ed838287c31dcec76a08c1c66371fdd7514c7
Julia Söderlund <julia.soderlund@yandex.ru>
2025-04-22 19:43:20 +0300

    fix: make language menu visible when logged out
    This is something that got fixed in a previous commit from July 17th,
    but it still crept back up on this branch somehow.
    
    Removed unused imports.

--- frontend/src/components/UserMenu/index.js
+++ frontend/src/components/UserMenu/index.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-// import { push } from 'connected-react-router'
-import { Box, ListItem, ListItemText, Grid } from '@mui/material'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Box, Grid } from '@mui/material'
 import * as AuthSelectors from 'reducers/auth/selectors'
 import JunctionTheme from 'junctionTheme.js'
 import Button from 'components/generic/Button'
 import UserAvatar from 'components/UserAvatar'
-import { useMyProfilePreview } from 'graphql/queries/userProfile'
-import { styled } from '@mui/system'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { useTranslation } from 'react-i18next'
 import LanguageMenu from 'components/LanguageMenu'
@@ -16,32 +13,9 @@ import LanguageMenu from 'components/LanguageMenu'
 export default () => {
     const { t } = useTranslation()
     const navigate = useNavigate()
-    const location = useLocation()
     const idTokenPayload = useSelector(AuthSelectors.getIdTokenPayload)
     const userId = idTokenPayload?.sub
-    const dispatch = useDispatch()
     // const classes = useStyles()
-
-    if (!userId) {
-        return (
-            <Box display="flex" flexDirection="row" alignItems="center">
-                <Grid container spacing={0}>
-                    <Grid item xs={12}>
-                        <Button
-                            onClick={() =>
-                                navigate('/login', {
-                                    state: { nextRoute: location.pathname },
-                                })
-                            }
-                            strong={true}
-                        >
-                            {t('Sign_in_')}
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Box>
-        )
-    }
 
     return (
         <Box className="tw-gap-2 tw-flex tw-flex-col md:tw-flex-row tw-items-center">
