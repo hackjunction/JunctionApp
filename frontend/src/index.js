c3d1e2bf5caf8bbc64cf26d495d68528b491e8c2
Julia Söderlund <julia.soderlund@yandex.ru>
2025-05-17 10:18:50 +0300

    Revert "fix: inject MUI styles before Tailwind"
    This reverts commit 00f87a138943337647630d0ef655529f36b130a9, since
    Tailwind will no longer be relevant.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker'
 import './i18n'
 import './styles/tailwind.css'
 import { CssBaseline } from '@mui/material'
-import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { CloudinaryContext } from 'cloudinary-react'
@@ -14,8 +14,8 @@ import WebFont from 'webfontloader'
 import Notifier from './notifier'
 import configureStore from 'reducers/configureStore'
 import config from 'constants/config'
+// import theme from './material-ui-theme'
 import theme from './junctionTheme'
-import './styles/tailwind.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -62,22 +62,20 @@ root.render(
                 includeOwnBody={true}
                 cloudName={config.CLOUDINARY_CLOUD_NAME}
             >
-                <StyledEngineProvider injectFirst>
-                    <ThemeProvider theme={theme}>
-                        <SnackbarProvider
-                            maxSnack={3}
-                            autoHideDuration={10000}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                        >
-                            <Notifier />
-                            <CssBaseline />
-                            <App />
-                        </SnackbarProvider>
-                    </ThemeProvider>
-                </StyledEngineProvider>
+                <ThemeProvider theme={theme}>
+                    <SnackbarProvider
+                        maxSnack={3}
+                        autoHideDuration={10000}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                    >
+                        <Notifier />
+                        <CssBaseline />
+                        <App />
+                    </SnackbarProvider>
+                </ThemeProvider>
             </CloudinaryContext>
         </PersistGate>
     </Provider>,
