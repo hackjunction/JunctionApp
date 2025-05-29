0e7963b9db15f5aadb202ee10fe40bfe31626dfc
Julia Söderlund <julia.soderlund@yandex.ru>
2025-05-29 22:21:47 +0300

    Remove unused IndexPage.js file

--- frontend/src/pages/_index/index.js
+++ frontend/src/pages/_index/index.js
@@ -3,13 +3,10 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
-import { useTranslation } from 'react-i18next'
 import * as AuthSelectors from '../../redux/auth/selectors'
 
 export default () => {
-    //TODO these shouldn't be queried. Events and organizations should be in the state
     const userIsAuthenticated = useSelector(AuthSelectors.isAuthenticated)
-    const { t } = useTranslation()
 
     return (
         <>
@@ -19,11 +16,6 @@ export default () => {
                 ) : (
                     <Redirect to="/home" />
                 )
-                // <Route
-                //     exact={true}
-                //     path={`${match.path}/home`}
-                //     component={IndexPage}
-                // />
             }
         </>
     )
