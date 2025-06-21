1352642647063a91e296d71f0369fb9ee8e68d94
Julia Söderlund <julia.soderlund@yandex.ru>
2025-06-21 19:01:37 +0300

    Update dashboard navbar to MUI v6

--- frontend/src/components/navbars/BasicNavBar/index.js
+++ frontend/src/components/navbars/BasicNavBar/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
+
+import { styled } from '@mui/material/styles'
+
+import WavePattern from 'assets/images/nawbar_waves.svg'
 import UserAvatar from 'components/UserAvatar'
 import LanguageMenu from 'components/LanguageMenu'
 
+const NavbarWrapper = styled('div')({
+    width: '100%',
+    background: `url(${WavePattern})`,
+    backgroundColor: 'black',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'end',
+    padding: '0.5rem',
+})
+
 const BasicNavBar = () => {
     return (
-        <div className="tw-w-full tw-p-2 tw-bg-wave-pattern tw-bg-black ">
-            <div className={'tw-flex tw-justify-end tw-items-center'}>
-                <LanguageMenu />
-                <UserAvatar />
-            </div>
-        </div>
+        <NavbarWrapper>
+            <LanguageMenu />
+            <UserAvatar />
+        </NavbarWrapper>
     )
 }
 
